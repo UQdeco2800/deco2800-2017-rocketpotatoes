efe134d8e52dca535867f04469f8821d88d78e25
Adam White <whitea0197@gmail.com>
2017-08-10 19:12:14 +1000

    I'm be fool, didn't add all files after a rename - fixed

--- potatoes/src/main/java/com/deco2800/potatoes/worlds/InitialWorld.java
+++ potatoes/src/main/java/com/deco2800/potatoes/worlds/InitialWorld.java
@@ -1,19 +1,12 @@
 package com.deco2800.potatoes.worlds;
 
 import java.util.Random;
-import java.util.Optional;
 
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.deco2800.potatoes.renderering.Renderable;
-import com.deco2800.potatoes.worlds.AbstractWorld;
-import com.deco2800.potatoes.entities.Tower;
-import com.deco2800.potatoes.entities.GoalPotate;
-import com.deco2800.potatoes.entities.Peon;
 import com.deco2800.potatoes.entities.Selectable;
 import com.deco2800.potatoes.entities.Squirrel;
-import com.deco2800.potatoes.entities.trees.*;
-import com.deco2800.potatoes.managers.GameManager;
-import com.deco2800.potatoes.util.WorldUtil;
+import com.deco2800.potatoes.entities.trees.ProjectileTree;
+import com.deco2800.potatoes.renderering.Renderable;
 
 /**
  * Initial world using preset world file.
@@ -39,7 +32,7 @@ public class InitialWorld extends AbstractWorld {
 		//this.addEntity(new Peon(7, 7, 0));
 //		this.addEntity(new Tower(8, 8, 0));
 		
-		this.addEntity(new projectileTree(10,10,0, 1, 1, 1, "tree"));
+		this.addEntity(new ProjectileTree(10,10,0, 1, 1, 1, "tree"));
 		
 		Random random = new Random();
 		for(int i = 0; i < 5; i++) {
