142c4bbcd77fc0e425651fdcd4bcf79fb8325d69
Ben-PC\BenJamin <benwildie@hotmail.com>
2017-08-10 15:54:17 +1000

    created upgrade stats class in projectile tree to make altering stats easier

--- potatoes/src/main/java/com/deco2800/potatoes/entities/trees/projectileTree.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/trees/projectileTree.java
@@ -5,10 +5,25 @@ import com.deco2800.potatoes.entities.Tickable;
 import com.deco2800.potatoes.entities.TimeEvent;
 import com.deco2800.potatoes.entities.trees.AbstractTree;
 
+class UpgradeStats {
+    private int hp;
+    private int speed;
+    public UpgradeStats(int hp, int speed) {
+        hp = hp;
+        speed = speed;
+    }
+    public int getHp(){
+        return hp;
+    }
+    public int getSpeed(){
+        return speed;
+    }
+}
 public class projectileTree extends AbstractTree implements Tickable{
     public int level;
     public int hp;
     public int speed;
+    private UpgradeStats level1 = new UpgradeStats(4,4);
     //public whateveraprojectileis projectile;
     public projectileTree(float posX, float posY, float posZ, float xLength, float yLength, float zLength,
                           String texture) {
@@ -24,7 +39,8 @@ public class projectileTree extends AbstractTree implements Tickable{
         // add upgrade numbers later
         switch (level) {
             case 1:
-                hp = 7;
+                hp = level1.getHp();
+                speed = level1.getSpeed();
                 break;
             case 2:
                 hp = 10;
@@ -33,3 +49,8 @@ public class projectileTree extends AbstractTree implements Tickable{
         level++;
     }
 }
+
+
+
+
+
