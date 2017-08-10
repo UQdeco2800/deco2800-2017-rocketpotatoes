705a6b9045ef88f8c4e8baac578e54d59e65ea0f
Michael Ruigrok <ruigrok.michael@gmail.com>
2017-08-10 14:20:51 +1000

    implemented the basic health Interface

---
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/Mortal.java
@@ -0,0 +1,35 @@
+package com.deco2800.potatoes.entities;
+
+/**
+ * An interface to handle heatlh, damage taken, healing, and death
+ * @author michaelruigrok
+ */
+public interface Mortal {
+
+	/**
+	 * @return the current health
+	 */
+	float getHealth();
+
+	/**
+	 * @return the max health
+	 */
+	float getMaxHealth();
+
+	/**
+	 * @param amount - the amount of health to subtract
+	 * @return iff health <= 0
+	 */
+	boolean damage(float amount);
+
+	/**
+	 * @param amount - the amount of health to refill
+	 * @return if health is now maxed out
+	 */
+	boolean heal(float amount);
+
+	/**
+	 * Perform the required death behaviour
+	 */ 
+	boolean deathHandler();
+}
