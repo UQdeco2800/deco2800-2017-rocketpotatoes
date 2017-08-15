891fc1d211a41b79f8e1fa0ab358566358c9c177
Tom Richardson <tgrkzus@gmail.com>
2017-08-15 17:36:50 +1000

    Camera centers on abitrary targets

--- potatoes/src/main/java/com/deco2800/potatoes/managers/CameraManager.java
+++ potatoes/src/main/java/com/deco2800/potatoes/managers/CameraManager.java
@@ -3,6 +3,7 @@ package com.deco2800.potatoes.managers;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
 import com.deco2800.potatoes.entities.AbstractEntity;
+import com.deco2800.potatoes.renderering.Render3D;
 
 public class CameraManager extends Manager{
 
@@ -30,40 +31,12 @@ public class CameraManager extends Manager{
 	 * Moves the camera to center on the target, if the camera has no target this method does nothing
 	 */
 	public void centerOnTarget() {
-		return;
 		if (camera != null) {
 			if (hasTarget()) {
-				//System.out.println(target.getPosX() + " : " + target.getPosY());
-				Vector2 xVector = new Vector2(
-						1,
-						0);
+				Vector2 isoPosition = Render3D.worldToScreenCoordinates(target.getPosX(), target.getPosY());
 
-				// Rotate x unit vector 60 degrees
-				xVector.rotate(60);
-
-				Vector2 yVector = new Vector2(
-						0,
-						1);
-
-				// Rotate y unit vector 30 degrees
-				yVector.rotate(30);
-
-				xVector.x *= target.getPosX() * 32;
-				xVector.y *= target.getPosX() * 32;
-
-				yVector.x *= target.getPosY() * 32;
-				yVector.y *= target.getPosY() * 32;
-
-				Vector2 newPos = new Vector2(0, 0);
-				newPos.add(xVector).add(yVector);
-
-
-				System.out.println(newPos.x + " : " + newPos.y);
-
-				camera.position.x = newPos.x;
-				camera.position.y = newPos.y;
-
-				//camera.project()
+				this.camera.position.x = isoPosition.x;
+				this.camera.position.y = isoPosition.y;
 			}
 		}
 	}
--- potatoes/src/main/java/com/deco2800/potatoes/renderering/Render3D.java
+++ potatoes/src/main/java/com/deco2800/potatoes/renderering/Render3D.java
@@ -6,20 +6,19 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.renderers.BatchTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.deco2800.potatoes.entities.AbstractEntity;
 import com.deco2800.potatoes.entities.HasProgress;
 import com.deco2800.potatoes.entities.Player;
 import com.deco2800.potatoes.managers.GameManager;
 import com.deco2800.potatoes.managers.MultiplayerManager;
 import com.deco2800.potatoes.managers.TextureManager;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.swing.text.html.parser.Entity;
-import java.awt.*;
-import java.util.*;
-import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 /**
  * A simple isometric renderer for DECO2800 games
@@ -43,15 +42,9 @@ public class Render3D implements Renderer {
             font.getData().setScale(1.0f);
         }
         Map<Integer, AbstractEntity> renderables = GameManager.get().getWorld().getEntities();
-        int worldLength = GameManager.get().getWorld().getLength();
-        int worldWidth = GameManager.get().getWorld().getWidth();
 
         int tileWidth = (int)GameManager.get().getWorld().getMap().getProperties().get("tilewidth");
         int tileHeight = (int)GameManager.get().getWorld().getMap().getProperties().get("tileheight");
-
-        float baseX = tileWidth*(worldWidth/2.0f - 0.5f); // bad
-
-        float baseY = -tileHeight/2*worldLength + tileHeight/2f; // good
 
         /* Tree map so we sort our entities properly */
         SortedMap<AbstractEntity, Integer> entities = new TreeMap<>();
@@ -71,33 +64,29 @@ public class Render3D implements Renderer {
             TextureManager reg = (TextureManager) GameManager.get().getManager(TextureManager.class);
             Texture tex = reg.getTexture(textureString);
 
-            float cartX = entity.getPosX();
-            float cartY = (worldWidth-1) - entity.getPosY();
-
-            float isoX = baseX + ((cartX - cartY) / 2.0f * tileWidth);
-            float isoY = baseY + ((cartX + cartY) / 2.0f) * tileHeight;
+            Vector2 isoPosition = worldToScreenCoordinates(entity.getPosX(), entity.getPosY());
 
             // We want to keep the aspect ratio of the image so...
             float aspect = (float)(tex.getWidth())/(float)(tileWidth);
 
-            batch.draw(tex, isoX, isoY, tileWidth*entity.getXRenderLength(),
+            batch.draw(tex, isoPosition.x, isoPosition.y, tileWidth*entity.getXRenderLength(),
                     (tex.getHeight()/aspect)*entity.getYRenderLength());
         }
 
         for (Map.Entry<AbstractEntity, Integer> e : entities.entrySet()) {
             AbstractEntity entity = e.getKey();
 
-            float cartX = entity.getPosX();
-            float cartY = (worldWidth-1) - entity.getPosY();
+            Vector2 isoPosition = worldToScreenCoordinates(entity.getPosX(), entity.getPosY());
 
-            float isoX = baseX + ((cartX - cartY) / 2.0f * tileWidth);
-            float isoY = baseY + ((cartX + cartY) / 2.0f) * tileHeight;
+            if (entity instanceof Player) {
+                System.out.println(entity + " : " + isoPosition.x + " : " + isoPosition.y);
+            }
 
 
             if (entity instanceof HasProgress && ((HasProgress) entity).showProgress()) {
                 font.setColor(Color.RED);
                 font.getData().setScale(1.0f);
-                font.draw(batch, String.format("%d%%", ((HasProgress) entity).getProgress()), isoX + tileWidth/2 - 10, isoY + 60);
+                font.draw(batch, String.format("%d%%", ((HasProgress) entity).getProgress()), isoPosition.x + tileWidth/2 - 10, isoPosition.y + 60);
             }
 
             MultiplayerManager m = (MultiplayerManager) GameManager.get().getManager(MultiplayerManager.class);
@@ -105,7 +94,7 @@ public class Render3D implements Renderer {
                 font.setColor(Color.WHITE);
                 font.getData().setScale(1.3f);
                 if (m.getID() == e.getValue()) { font.setColor(Color.BLUE); }
-                font.draw(batch, String.format("%s", m.getClients().get(e.getValue())), isoX + tileWidth/2 - 10, isoY + 70);
+                font.draw(batch, String.format("%s", m.getClients().get(e.getValue())), isoPosition.x + tileWidth/2 - 10, isoPosition.y + 70);
 
             }
         }
@@ -139,5 +128,40 @@ public class Render3D implements Renderer {
     @Override
     public BatchTiledMapRenderer getTileRenderer(SpriteBatch batch) {
         return new IsometricTiledMapRenderer(GameManager.get().getWorld().getMap(), 1, batch);
+    }
+
+
+    /**
+     * Transforms world coordinates to screen coordinates for rendering.
+     * @param x x coord in the world
+     * @param y y coord in the world
+     * @return a Vector2 with the screen coordinates
+     */
+    public static Vector2 worldToScreenCoordinates(float x, float y) {
+        int worldLength = GameManager.get().getWorld().getLength();
+        int worldWidth = GameManager.get().getWorld().getWidth();
+
+        int tileWidth = (int)GameManager.get().getWorld().getMap().getProperties().get("tilewidth");
+        int tileHeight = (int)GameManager.get().getWorld().getMap().getProperties().get("tileheight");
+
+        float baseX = tileWidth*(worldWidth/2.0f - 0.5f); // bad
+        float baseY = -tileHeight/2*worldLength + tileHeight/2f; // good
+
+        float cartX = x;
+        float cartY = (worldWidth-1) - y;
+
+        float isoX = baseX + ((cartX - cartY) / 2.0f * tileWidth);
+        float isoY = baseY + ((cartX + cartY) / 2.0f) * tileHeight;
+
+        return new Vector2(isoX, isoY);
+    }
+
+    /**
+     * Transforms world coordinates to screen coordinates for rendering.
+     * @param p Vector2 with the world coords
+     * @return a Vector2 with the screen coordinates
+     */
+    public static Vector2 worldToScreenCoordinates(Vector2 p) {
+        return worldToScreenCoordinates(p.x, p.y);
     }
 }
