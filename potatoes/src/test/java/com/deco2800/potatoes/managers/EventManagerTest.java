8d73d00e57c8927dead69c3a96b3fdd836c40bb2
Adam White <whitea0197@gmail.com>
2017-08-19 21:55:23 +1000

    Added event manager and tests

---
+++ potatoes/src/test/java/com/deco2800/potatoes/managers/EventManagerTest.java
@@ -0,0 +1,80 @@
+package com.deco2800.potatoes.managers;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.deco2800.potatoes.TimeEventTest.TestTimeEvent;
+import com.deco2800.potatoes.entities.Tickable;
+
+public class EventManagerTest {
+	EventManager eventManager;
+	List<TestTimeEvent<Tickable>> events;
+	Tickable tickable1;
+	Tickable tickable2;
+
+	@Before
+	public void testSetup() {
+		tickable1 = new Tickable() {
+			@Override
+			public void onTick(long time) {
+			}
+		};
+		tickable2 = new Tickable() {
+			@Override
+			public void onTick(long time) {
+			}
+		};
+		eventManager = new EventManager();
+		events = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			events.add(new TestTimeEvent<>());
+			eventManager.registerEvent(i < 3 ? tickable1 : tickable2, events.get(i));
+		}
+	}
+
+	@Test
+	public void testUnregisterEvent() {
+		eventManager.unregisterEvent(tickable1, events.get(0));
+		eventManager.unregisterEvent(tickable2, events.get(3));
+		eventManager.tickAll(1);
+		int[] results = { 0, 1, 1, 0, 1 };
+		int[] actionCounts = new int[5];
+		for (int i = 0; i < events.size(); i++) {
+			actionCounts[i] = events.get(i).getActionCount();
+
+		}
+		assertArrayEquals("Unregistering event did", actionCounts, results);
+	}
+
+	@Test
+	public void testUnregisterAll() {
+		eventManager.unregisterAll(tickable1);
+		eventManager.tickAll(1);
+		int[] results = { 0, 0, 0, 1, 1 };
+		int[] actionCounts = new int[5];
+		for (int i = 0; i < events.size(); i++) {
+			actionCounts[i] = events.get(i).getActionCount();
+
+		}
+		// Expand on this description
+		assertArrayEquals("Ticked events did not match expected", actionCounts, results);
+	}
+
+	@Test
+	public void testTickAll() {
+		eventManager.tickAll(1);
+		int[] results = { 1, 1, 1, 1, 1 };
+		int[] actionCounts = new int[5];
+		for (int i = 0; i < events.size(); i++) {
+			actionCounts[i] = events.get(i).getActionCount();
+
+		}
+		assertArrayEquals("Ticking all events didn't run all actions", actionCounts, results);
+	}
+
+}
