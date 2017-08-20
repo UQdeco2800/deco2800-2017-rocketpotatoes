93eb23743e8d732df5e05d90891cd84e8405b2d9
jholder96 <jholder691@gmail.com>
2017-08-20 15:47:33 +1000

    Change Resource from an interfce to a class. Updated SeedResource and FoodResource classes.

--- potatoes/src/main/java/com/deco2800/potatoes/entities/FoodResource.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/FoodResource.java
@@ -1,10 +1,6 @@
 package com.deco2800.potatoes.entities;
 
-public class FoodResource implements Resource {
-	/*
-	 * Unique ID assigned to each resource.
-	 */
-	private int type;
+public class FoodResource extends Resource {
 	/*
 	 * Unique name for each resource.
 	 */
@@ -13,29 +9,42 @@ public class FoodResource implements Resource {
 	
 	/**
 	 * <p>
-	 * Creates a new instance of the class. Also assigns it the unique ID and
-	 * name of the resource.
+	 * Creates a new instance of the class and assigns the name of 
+	 * the resource.
+	 * </p>
+	 * 
+	 * <p>
+	 * Only to be used when the instance of the resource isn't appearing
+	 * on the map.
 	 * </p>
 	 */
 	public FoodResource() {
-		type = 2;
 		name = "Food";
 	}
-
-	@Override
+	
 	/**
 	 * <p>
-	 * Returns the unique ID for a resource. For seeds this is 1.
+	 * Creates a new instance of the class and assigns the name of 
+	 * the resource.
 	 * </p>
 	 * 
-	 * @return ID
-	 * 		The numerical representation of the resource.
+	 * <p>
+	 * Also sets the location of the resource on the map
+	 * </p>
+	 * 
+	 * @param xCoord
+	 * 		The x location of the resource.
+	 * @param yCoord
+	 * 		The y location of the resource.
+	 * @param zCoord
+	 * 		The z location of the resource.
 	 */
-	public int getResourceType() {
-		return type;
+	public FoodResource(int xCoord, int yCoord, int zCoord) {
+		name = "Food";
+		
+		this.move(xCoord, yCoord, zCoord);
 	}
-	
-	@Override
+
 	/**
 	 * <p>
 	 * Returns the string representation of the resource.
--- potatoes/src/main/java/com/deco2800/potatoes/entities/Resource.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/Resource.java
@@ -1,43 +1,97 @@
 package com.deco2800.potatoes.entities;
 
-public interface Resource {
-	// possible additions:
-	// 		- location data
-	//		- texture data
+public class Resource {
+	/*
+	 * The location of the resource on the x axis
+	 */
+	private int xValue;
+	/*
+	 * The location of the resource on the y axis
+	 */
+	private int yValue;
+	/*
+	 * The location of the resource on the z axis
+	 */
+	private int zValue;
 	
+	/**
+	 * <p>Returns the x location of the resource</p>
+	 * 
+	 * @return xValue
+	 * 		The x location of the resource
+	 */
+	public int getX() {
+		return xValue;
+	}
+	
+	/**
+	 * <p>Returns the y location of the resource</p>
+	 * 
+	 * @return yValue
+	 * 		The y location of the resource
+	 */
+	public int getY() {
+		return yValue;
+	}
+	
+	/**
+	 * <p>Returns the z location of the resource</p>
+	 * 
+	 * @return zValue
+	 * 		The z location of the resource
+	 */
+	public int getZ() {
+		return zValue;
+	}
+	
+	/**
+	 * <p>Returns the three coordinates of the resource.</p>
+	 * 
+	 * @return coordinates
+	 * 		The x, y and z coordinates for the resource in that order.
+	 */
+	public int[] getLocation() {
+		int[] coordinates = new int[] {xValue, yValue, zValue};
+		
+		return coordinates;
+	}
+	
+	/**
+	 * <p>Updates the coordinates of the resource on the map.</p>
+	 * 
+	 * <p>
+	 * Can simply call with getX(), getY() and getZ() to update only one value.
+	 * </p>
+	 * 
+	 * @param xNew
+	 * 		The new x coordinate.
+	 * @param yNew
+	 * 		The new y coordinate.
+	 * @param zNew
+	 * 		The new z coordinate.
+	 */
+	public void move(int xNew, int yNew, int zNew) {
+		xValue = xNew;
+		yValue = yNew;
+		zValue = zNew;
+	}
 	
 	/**
 	 * <p>
-	 * Returns the ID of the resource.
+	 * Removes the resource from the map.
 	 * </p>
-	 * 
-	 * <p>
-	 * Method to be implemented by each resource class.
-	 * </p>
-	 * 
-	 * @return ID
-	 * 		The numerical representation of the resource.
-	 * 		Core game resources: 0xx. Eg. 1 for Seeds
-	 * 		Offensive resources: 1xx. Eg. possibly 101 for arrows.
-	 * 		Defensive resources: 2xx. Eg. possibly 204 for a healing item.
-	 * 		Passive items: 3xx. Eg possibly 345 for a shield.
-	 * 
-	 * 		Extend as needed.
 	 */
-	public int getResourceType();
+	public void delete() {
+		//to be completed after further discussion
+	}
 	
 	/**
 	 * <p>
-	 * Returns the string representation of the resource.
+	 * Returns the texture data for the resource.
 	 * </p>
-	 * 
-	 * <p>
-	 * Method to be implemented by each resource class.
-	 * </p>
-	 * 
-	 * @return string
-	 * 		The string representation of the resource.
 	 */
-	public String toString();
+	public void getTexture() {
+		//to be completed after further discussion
+	}
 
 }
