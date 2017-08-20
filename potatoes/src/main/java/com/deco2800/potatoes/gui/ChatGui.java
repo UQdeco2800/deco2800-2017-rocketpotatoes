a323211c28cdab7c416ce492aad8a53130e3358c
Tom Richardson <tgrkzus@gmail.com>
2017-08-20 22:50:10 +1000

    Added custom libgdx List implementation specific to labels. Removes the selection functionality and allows individual labels to have their own colors which aren't set by the parent globally|

--- potatoes/src/main/java/com/deco2800/potatoes/gui/ChatGui.java
+++ potatoes/src/main/java/com/deco2800/potatoes/gui/ChatGui.java
@@ -2,39 +2,58 @@ package com.deco2800.potatoes.gui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ArraySelection;
+import com.badlogic.gdx.scenes.scene2d.utils.Cullable;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+import com.badlogic.gdx.utils.Pools;
 
 public class ChatGui extends Gui {
     private Skin uiSkin;
     private Table table;
     private ScrollPane chatContainer;
-    private List<Label> textArea;
+    private ChatList textArea;
 
     /**
      * Initializes this ChatGui
      */
     public ChatGui(Stage stage) {
         hidden = false;
-        // Make window, with the given skin
         uiSkin = new Skin(Gdx.files.internal("uiskin.json"));
 
+        // Our table contains our entire chat gui
         table = new Table(uiSkin);
-        textArea = new List<>(uiSkin);
-        Label t = new Label("Hey!", uiSkin);
-        t.setName("[TOM]");
-        t.setColor(Color.RED);
-        t.set
-        textArea.setItems(t);
-        textArea.setSelectedIndex(-1);
+
+        // List of messages, with custom draw method so we have label specific
+        textArea = new ChatList(uiSkin);
+
+        // Remove our listeners since we don't want our messages to be clickable
+        for (EventListener e : textArea.getListeners()) {
+            textArea.removeListener(e);
+        }
+
+        // Scrollpane containing the list of messages
         chatContainer = new ScrollPane(textArea, uiSkin);
+        Label t = new Label("Hey!", uiSkin);
+        t.setName("[Tom] ");
+        t.setColor(Color.RED);
+
+
+        textArea.setItems(t);
         chatContainer.setForceScroll(false, true);
         chatContainer.setScrollBarPositions(false, true);
         chatContainer.setFadeScrollBars(false);
         chatContainer.pack();
-
         resetGui(stage);
-        stage.addActor(table);  
+        stage.addActor(table);
     }
 
     /**
@@ -56,5 +75,174 @@ public class ChatGui extends Gui {
         table.getColor().a = 0.8f;
         table.setPosition(0, 0);
         table.pack();
+    }
+
+    /**
+     * Add's a message to the chat box.
+     *
+     * @param name  the name/sender of the message (e.g. "System", "Bob", "Jimmy"
+     * @param text  the contents of the message (e.g. "tomgr is cool"
+     * @param color the gdx defined colour
+     */
+    public void addMessage(String name, String text, Color color) {
+
+    }
+
+    /** Tweaked list element so we can colour specific elements, and don't listen for click events
+     * Note: I've just ripped 90% of this off the libgdx implementation
+     * Also Note: The style and font size needs to be the same as the Labels contained within or this will break
+     */
+    private class ChatList extends Widget implements Cullable {
+        private com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle style;
+        final Array<Label> items = new Array();
+        private Rectangle cullingArea;
+        private float prefWidth, prefHeight;
+        private float itemHeight;
+        private float textOffsetX, textOffsetY;
+
+        public ChatList(Skin skin) {
+            this(skin.get(com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle.class));
+        }
+
+        public ChatList(Skin skin, String styleName) {
+            this(skin.get(styleName, com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle.class));
+        }
+
+        public ChatList(com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle style) {
+            setStyle(style);
+            setSize(getPrefWidth(), getPrefHeight());
+        }
+
+        public void setStyle(com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle style) {
+            if (style == null) throw new IllegalArgumentException("style cannot be null.");
+            this.style = style;
+            invalidateHierarchy();
+        }
+
+        public void layout() {
+            final BitmapFont font = style.font;
+            final Drawable selectedDrawable = style.selection;
+
+            itemHeight = font.getCapHeight() - font.getDescent() * 2;
+            itemHeight += selectedDrawable.getTopHeight() + selectedDrawable.getBottomHeight();
+
+            textOffsetX = selectedDrawable.getLeftWidth();
+            textOffsetY = selectedDrawable.getTopHeight() - font.getDescent();
+
+            prefWidth = 0;
+            Pool<GlyphLayout> layoutPool = Pools.get(GlyphLayout.class);
+            GlyphLayout layout = layoutPool.obtain();
+            for (int i = 0; i < items.size; i++) {
+                layout.setText(font, toString(items.get(i)));
+                prefWidth = Math.max(layout.width, prefWidth);
+            }
+            layoutPool.free(layout);
+            prefWidth += selectedDrawable.getLeftWidth() + selectedDrawable.getRightWidth();
+            prefHeight = items.size * itemHeight;
+
+            Drawable background = style.background;
+            if (background != null) {
+                prefWidth += background.getLeftWidth() + background.getRightWidth();
+                prefHeight += background.getTopHeight() + background.getBottomHeight();
+            }
+        }
+
+        @Override
+        public void draw(Batch batch, float parentAlpha) {
+            validate();
+
+            BitmapFont font = style.font;
+
+            Color color = getColor();
+            batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
+
+            float x = getX(), y = getY(), width = getWidth(), height = getHeight();
+            float itemY = height;
+
+            Drawable background = style.background;
+            if (background != null) {
+                background.draw(batch, x, y, width, height);
+                float leftWidth = background.getLeftWidth();
+                x += leftWidth;
+                itemY -= background.getTopHeight();
+                width -= leftWidth + background.getRightWidth();
+            }
+
+            for (int i = 0; i < items.size; i++) {
+                if (cullingArea == null || (itemY - itemHeight <= cullingArea.y + cullingArea.height && itemY >= cullingArea.y)) {
+                    Label item = items.get(i);
+                    font.setColor(item.getColor().r, item.getColor().g, item.getColor().b, item.getColor().a * parentAlpha);
+                    drawItem(batch, font, i, item, x + textOffsetX, y + itemY - textOffsetY);
+                } else if (itemY < cullingArea.y) {
+                    break;
+                }
+                itemY -= itemHeight;
+            }
+        }
+
+        protected GlyphLayout drawItem(Batch batch, BitmapFont font, int index, Label item, float x, float y) {
+            return font.draw(batch, toString(item), x, y);
+        }
+
+        public void setItems(Label... newItems) {
+            if (newItems == null) throw new IllegalArgumentException("newItems cannot be null.");
+            float oldPrefWidth = getPrefWidth(), oldPrefHeight = getPrefHeight();
+
+            items.clear();
+            items.addAll(newItems);
+
+            invalidate();
+            if (oldPrefWidth != getPrefWidth() || oldPrefHeight != getPrefHeight()) invalidateHierarchy();
+        }
+
+        /**
+         * Sets the items visible in the list, clearing the selection if it is no longer valid. If a selection is
+         * {@link ArraySelection#getRequired()}, the first item is selected.
+         */
+        public void setItems(Array newItems) {
+            if (newItems == null) throw new IllegalArgumentException("newItems cannot be null.");
+            float oldPrefWidth = getPrefWidth(), oldPrefHeight = getPrefHeight();
+
+            items.clear();
+            items.addAll(newItems);
+
+            invalidate();
+            if (oldPrefWidth != getPrefWidth() || oldPrefHeight != getPrefHeight()) invalidateHierarchy();
+        }
+
+        public void clearItems() {
+            if (items.size == 0) return;
+            items.clear();
+            invalidateHierarchy();
+        }
+
+        /**
+         * Returns the internal items array. If modified, {@link #setItems(Array)} must be called to reflect the changes.
+         */
+        public Array<Label> getItems() {
+            return items;
+        }
+
+        public float getItemHeight() {
+            return itemHeight;
+        }
+
+        public float getPrefWidth() {
+            validate();
+            return prefWidth;
+        }
+
+        public float getPrefHeight() {
+            validate();
+            return prefHeight;
+        }
+
+        protected String toString(Label obj) {
+            return obj.toString();
+        }
+
+        public void setCullingArea(Rectangle cullingArea) {
+            this.cullingArea = cullingArea;
+        }
     }
 }
