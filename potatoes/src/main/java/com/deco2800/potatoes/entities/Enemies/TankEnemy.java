e6335bbd587f952351fb6699a47b4bf78daf5d10
Qishi Zheng <vincezheng4265@gmail.com>
2017-08-22 17:07:17 +1000

    Created TankEnemy as a sub-type of enemy

---
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/Enemies/TankEnemy.java
@@ -0,0 +1,23 @@
+package com.deco2800.potatoes.entities.Enemies;
+
+import java.util.Map;
+import java.util.Random;
+
+import com.deco2800.potatoes.entities.*;
+import com.deco2800.potatoes.managers.GameManager;
+import com.deco2800.potatoes.managers.PlayerManager;
+import com.deco2800.potatoes.managers.SoundManager;
+import com.deco2800.potatoes.util.Box3D;
+
+import java.util.Map;
+
+/**
+ * Sub-class of AbstractEnemy
+ * Tank Enemy has more health, slower move speed, stronger attack power
+ *
+ */
+public class TankEnemy extends AbstractEnemy implements Tickable, HasProgress{
+
+	
+	
+}
