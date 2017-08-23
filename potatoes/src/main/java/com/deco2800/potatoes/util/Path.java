c6386f1a5cd40eddda69cae01f9b0f7c0540bfd2
Andrew Hine <andhine87@gmail.com>
2017-08-23 13:25:53 +1000

    Enemy movement architecture planning.

---
+++ potatoes/src/main/java/com/deco2800/potatoes/util/Path.java
@@ -0,0 +1,19 @@
+package com.deco2800.potatoes.util;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class Path {
+
+    /**
+     * @param nodes
+     */
+    public Path(List<Box3D> nodes) {
+
+    }
+
+
+
+}
