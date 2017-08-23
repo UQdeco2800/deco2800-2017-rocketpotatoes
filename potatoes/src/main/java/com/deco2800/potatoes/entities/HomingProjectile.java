33260d9c97a8e04299548475d61efff62d5fea32
Ding Wei Sheng <weisheng5566@gmail.com>
2017-08-23 12:58:35 +1000

    Added damage over time for enemies which are in AOE circle. Pending fix: only single target is affected

--- potatoes/src/main/java/com/deco2800/potatoes/entities/HomingProjectile.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/HomingProjectile.java
@@ -91,9 +91,9 @@ public class HomingProjectile extends Projectile {
 				GameManager.get().getWorld().removeEntity(this);
 				float AOE_width = 5f;
 				float AOE_height = 2f;
-				ExplosionProjectile exp = new ExplosionProjectile(goalX - (AOE_width / 2), goalY - (AOE_height / 2), 0,
-						AOE_width, AOE_height, 0, AOE_width, AOE_height, "aoe1");
-				// GameManager.get().getWorld().addEntity(exp);
+//				ExplosionProjectile exp = new ExplosionProjectile(goalX - (AOE_width / 2), goalY - (AOE_height / 2), 0,
+//						AOE_width, AOE_height, 0, AOE_width, AOE_height, "aoe1");
+//				 GameManager.get().getWorld().addEntity(exp);
 
 				return;
 			}
