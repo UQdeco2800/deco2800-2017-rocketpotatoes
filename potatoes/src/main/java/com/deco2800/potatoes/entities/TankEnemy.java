56f7143066faf97063b4c341faf545d8f629a7fd
Qishi Zheng <vincezheng4265@gmail.com>
2017-08-24 18:33:03 +1000

    added a tankEnemy to the game

---
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/TankEnemy.java
@@ -0,0 +1,118 @@
+package com.deco2800.potatoes.entities;
+
+import java.util.Map;
+import java.util.Random;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.deco2800.potatoes.entities.AbstractEntity;
+import com.deco2800.potatoes.entities.Tickable;
+import com.deco2800.potatoes.managers.GameManager;
+import com.deco2800.potatoes.managers.PlayerManager;
+import com.deco2800.potatoes.managers.SoundManager;
+import com.deco2800.potatoes.util.Box3D;
+
+/**
+ * A generic player instance for the game
+ */
+public class TankEnemy extends EnemyEntity implements Tickable, HasProgress, ProgressBar{
+	
+	private static final transient String TEXTURE = "potate";
+	private static final transient float HEALTH = 200f;
+	private transient Random random = new Random();
+
+	private float speed = 0.06f;
+
+	public TankEnemy() {
+		super(0, 0, 0, 1f, 1f, 1f, 1f, 1f, TEXTURE, HEALTH);
+	}
+
+	public TankEnemy(float posX, float posY, float posZ) {
+		super(posX, posY, posZ, 1f, 1f, 1f, 1f, 1f, TEXTURE, HEALTH);
+		//this.setTexture("squirrel");
+		//this.random = new Random();
+	}
+
+	@Override
+	public void onTick(long i) {
+
+		PlayerManager playerManager = (PlayerManager) GameManager.get().getManager(PlayerManager.class);
+		SoundManager soundManager = (SoundManager) GameManager.get().getManager(SoundManager.class);
+
+//		float goalX = playerManager.getPlayer().getPosX() + random.nextFloat() * 6 - 3;
+//		float goalY = playerManager.getPlayer().getPosY() + random.nextFloat() * 6 - 3;
+
+		//The X and Y position of the player without random floats generated
+		float goalX = playerManager.getPlayer().getPosX() ;
+		float goalY = playerManager.getPlayer().getPosY() + random.nextFloat() * 6 -3;
+		
+
+		if(this.distance(playerManager.getPlayer()) < speed) {
+			this.setPosX(goalX);
+			this.setPosY(goalY);
+			return;
+		}
+
+		float deltaX = getPosX() - goalX;
+		float deltaY = getPosY() - goalY;
+
+		float angle = (float)(Math.atan2(deltaY, deltaX)) + (float)(Math.PI);
+
+
+
+		float changeX = (float)(speed * Math.cos(angle));
+		float changeY = (float)(speed * Math.sin(angle));
+
+		Box3D newPos = getBox3D();
+
+		newPos.setX(getPosX() + changeX);
+		newPos.setY(getPosY() + changeY);
+
+		 
+		Map<Integer, AbstractEntity> entities = GameManager.get().getWorld().getEntities();
+		boolean collided = false;
+		for (AbstractEntity entity : entities.values()) {
+			if (!this.equals(entity) && !(entity instanceof Projectile) && newPos.overlaps(entity.getBox3D()) ) {
+				if(entity instanceof Player) {
+					//soundManager.playSound("ree1.wav");
+				}
+				collided = true;
+			}
+		}
+
+		if (!collided) {
+			setPosX(getPosX() + changeX);
+			setPosY(getPosY() + changeY);
+			//Squirrel changes direction when moving towards player.
+
+			if(this.getPosX()>goalX){
+				this.setTexture(TEXTURE);
+			}
+			else{
+				this.setTexture(TEXTURE);
+			}
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "Squirrel";
+	}
+
+	@Override
+	public void setProgressBar(AbstractEntity entity, Texture progressBar, SpriteBatch batch, int xLength, int yLength) {
+		if (health > 60) {
+			batch.setColor(Color.GREEN);
+		} else if (health > 20) {
+			batch.setColor(Color.ORANGE);
+		} else {
+			batch.setColor(Color.RED);
+		}
+
+		batch.draw(progressBar, xLength, yLength, health/3, 5);
+		batch.setColor(Color.WHITE);
+		
+	}
+
+}
