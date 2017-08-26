8751399b6bed08e1fb997801718dd8feb7755769
Joseph Lay <josephlay_@hotmail.com>
2017-08-26 16:36:40 +1000

    add max health to progressbar

--- potatoes/src/main/java/com/deco2800/potatoes/entities/ProgressBarEntity.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/ProgressBarEntity.java
@@ -46,7 +46,7 @@ public class ProgressBarEntity implements ProgressBar {
 	@Override
 	public Color getColour(int currentHealth, int maxHealth) {
 		// based on percentage of health
-		int ratio = 100 / (colours.size() - 1);
+		int ratio = maxHealth / (colours.size() - 1);
 		return colours.get(currentHealth / ratio);	}
 
 	@Override
