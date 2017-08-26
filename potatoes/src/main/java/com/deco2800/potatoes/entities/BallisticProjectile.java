d6f133ef067b5218a33a1a48efb5dec3b91516ce
Ding Wei Sheng <weisheng5566@gmail.com>
2017-08-27 05:28:51 +1000

    Added custom damages for aoe effect Added explosion effect class Modified projectile sprites Change projectile sprites Added comments Tidied up some codes for ease of use Removed aoe sprites from placeholderasset (using sprites from projectiles folder instead) Modified Projectile test file Modified texture manager according to what was changed in sprites (eg.path to files)

--- potatoes/src/main/java/com/deco2800/potatoes/entities/BallisticProjectile.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/BallisticProjectile.java
@@ -8,175 +8,201 @@ import com.deco2800.potatoes.managers.GameManager;
 
 public class BallisticProjectile extends Projectile {
 
-	private final static transient String TEXTURE = "projectile";
-	private static float DAMAGE = 1;
-	private static float RANGE;
+    private final static transient String TEXTURE = "rocket1";
+    private static float DAMAGE = 1;
+    private static float aoeDAMAGE = 1;
+    private static float RANGE;
+    private int rocketEffectTimer;
+    private int rocketCurrentSpriteIndexCount;
+    private String[] rocketSpriteArray = {"rocket1", "rocket2", "rocket3"};
+//    private String[] expSpriteArray = {"exp1","exp2","exp3"};
+//    private int expEffectTimer = 0;
+//    private int expCurrentSpriteIndexCount = 0;
 
-	private float goalX;
-	private float goalY;
-	private float goalZ;
-	
-	private int rotateAngle = 0;
+    /**
+     * Width and height of AOE sprite. NOTE: (height < width) to give isometric illusion
+     *
+     * @param AOE_width      Width of AOE sprite
+     * @param AOE_height     Height of AOE sprite
+     */
+    private final static float AOE_width = 5f;
+    private final static float AOE_height = 2f;
 
-	private final float speed = 0.2f;
-	private Optional<AbstractEntity> mainTarget;
-	private float changeX;
-	private float changeY;
-	private float changeZ;
-	
-	private boolean maxRange = false;
+    private float goalX;
+    private float goalY;
+    private float goalZ;
 
-	public BallisticProjectile() {
-		// empty for serialization
-	}
+    private int rotateAngle = 0;
 
-	/**
-	 * Creates a new Ballistic Projectile. Ballistic Projectiles do not change
-	 * direction once fired. The initial direction is based on the direction to the
-	 * closest entity
-	 * 
-	 * @param posX
-	 *            x start position
-	 * @param posY
-	 *            y start position
-	 * @param posZ
-	 *            z start position
-	 * @param target
-	 *            Entity target object
-	 * @param RANGE
-	 *            Projectile range
-	 * @param DAMAGE
-	 *            Projectile damage
-	 */
-	public BallisticProjectile(float posX, float posY, float posZ, Optional<AbstractEntity> target, float RANGE,
-			float DAMAGE) {
-		super(posX, posY, posZ, TEXTURE);
-		this.DAMAGE = DAMAGE;
-		this.mainTarget = target;
-		this.goalX = target.get().getPosX();
-		this.goalY = target.get().getPosY();
-		this.goalZ = target.get().getPosZ();
+    private final float speed = 0.2f;
+    private Optional<AbstractEntity> mainTarget;
+    private float changeX;
+    private float changeY;
+    private float changeZ;
 
-		this.RANGE = RANGE;
+    private boolean maxRange = false;
 
-		float deltaX = getPosX() - goalX;
-		float deltaY = getPosY() - goalY;
+    public BallisticProjectile() {
+        // empty for serialization
+        rotateAngle = 0;
+        DAMAGE = 1;
+        aoeDAMAGE = 1;
+        maxRange = false;
+    }
 
-		float angle = (float) (Math.atan2(deltaY, deltaX)) + (float) (Math.PI);
+    /**
+     * Creates a new Ballistic Projectile. Ballistic Projectiles do not change
+     * direction once fired. The initial direction is based on the direction to the
+     * closest entity
+     *
+     * @param posX      x start position
+     * @param posY      y start position
+     * @param posZ      z start position
+     * @param target    Entity target object
+     * @param RANGE     Projectile range
+     * @param DAMAGE    Projectile damage
+     * @param aoeDAMAGE AOE damage
+     */
+    public BallisticProjectile(float posX, float posY, float posZ, Optional<AbstractEntity> target, float RANGE,
+                               float DAMAGE, float aoeDAMAGE) {
+        super(posX, posY, posZ, 1, 1,TEXTURE);
+        this.DAMAGE = DAMAGE;
+        this.mainTarget = target;
+        this.goalX = target.get().getPosX();
+        this.goalY = target.get().getPosY();
+        this.goalZ = target.get().getPosZ();
+        this.aoeDAMAGE = aoeDAMAGE;
+        this.RANGE = RANGE;
 
-		changeX = (float) (speed * Math.cos(angle));
-		changeY = (float) (speed * Math.sin(angle));
-		// TODO: add changeZ
-		
-		rotateAngle = (int) ((angle * 180 / Math.PI) + 45 + 90);
+        float deltaX = getPosX() - goalX;
+        float deltaY = getPosY() - goalY;
 
-	}
-	
-	/**
-	 * Creates a new Ballistic Projectile. Ballistic Projectiles do not change
-	 * direction once fired. The initial direction is based on the direction to the
-	 * closest entity
-	 * 
-	 * @param posX
-	 *            x start position
-	 * @param posY
-	 *            y start position
-	 * @param posZ
-	 *            z start position
-	 * @param fPosX
-	 *            target x position
-	 * @param fPosY
-	 *            target y position
-	 * @param fPosZ
-	 *            target z position
-	 * @param RANGE
-	 *            Projectile range
-	 * @param DAMAGE
-	 *            Projectile damage
-	 */
-	public BallisticProjectile(float posX, float posY, float posZ, float fPosX, float fPosY, float fPosZ, float RANGE,
-			float DAMAGE) {
-		super(posX, posY, posZ, TEXTURE);
-		this.DAMAGE = DAMAGE;
-		
-		this.goalX = fPosX;
-		this.goalY = fPosY;
-		this.goalZ = fPosZ;
+        float angle = (float) (Math.atan2(deltaY, deltaX)) + (float) (Math.PI);
 
-		this.RANGE = RANGE;
+        changeX = (float) (speed * Math.cos(angle));
+        changeY = (float) (speed * Math.sin(angle));
+        // TODO: add changeZ
 
-		float deltaX = getPosX() - goalX;
-		float deltaY = getPosY() - goalY;
-		float deltaZ = getPosZ() - goalZ;
+        rotateAngle = (int) ((angle * 180 / Math.PI) + 45 + 90);
 
-		float angle = (float) (Math.atan2(deltaY, deltaX)) + (float) (Math.PI);
-		
-		changeX = (float) (speed * Math.cos(angle));
-		changeY = (float) (speed * Math.sin(angle));
-		// TODO: add changeZ
-		
-		rotateAngle = (int) ((angle * 180 / Math.PI) + 45 + 90);
-	}
-	
-	public int rotateAngle() {
-		return rotateAngle;
-	}
+    }
 
-	@Override
-	public void onTick(long time) {
+    /**
+     * Creates a new Ballistic Projectile. Ballistic Projectiles do not change
+     * direction once fired. The initial direction is based on the direction to the
+     * closest entity
+     *
+     * @param posX      x start position
+     * @param posY      y start position
+     * @param posZ      z start position
+     * @param fPosX     target x position
+     * @param fPosY     target y position
+     * @param fPosZ     target z position
+     * @param RANGE     Projectile range
+     * @param DAMAGE    Projectile hit damage
+     * @param aoeDAMAGE AOE damage
+     */
+    public BallisticProjectile(float posX, float posY, float posZ, float fPosX, float fPosY, float fPosZ, float RANGE,
+                               float DAMAGE, float aoeDAMAGE) {
+        super(posX, posY, posZ, TEXTURE);
+        this.DAMAGE = DAMAGE;
+        this.aoeDAMAGE = aoeDAMAGE;
+        this.goalX = fPosX;
+        this.goalY = fPosY;
+        this.goalZ = fPosZ;
 
-		updatePos();
+        this.RANGE = RANGE;
 
-		Collection<AbstractEntity> entities = GameManager.get().getWorld().getEntities().values();
-		for (AbstractEntity entity : entities) {
+        float deltaX = getPosX() - goalX;
+        float deltaY = getPosY() - goalY;
+        float deltaZ = getPosZ() - goalZ;
 
-			if (entity instanceof EnemyEntity && this.collidesWith(entity)) {
-				((EnemyEntity) entity).getShot(this);
-				GameManager.get().getWorld().removeEntity(this);
+        float angle = (float) (Math.atan2(deltaY, deltaX)) + (float) (Math.PI);
 
-				/**
-				 * Width of AOE sprite. NOTE: (height < width) to give isometric illusion
-				 */
-				float AOE_width = 5f;
-				float AOE_height = 2f;
-				
-				/**
-				 * Spawn explosion when projectile hits entity
-				 */
-				ExplosionProjectile exp = new ExplosionProjectile(goalX - (AOE_width / 2), goalY - (AOE_height / 2), 0,
-						AOE_width+10, AOE_height+10, 0, AOE_width, AOE_height, 1);
-				GameManager.get().getWorld().addEntity(exp);
+        changeX = (float) (speed * Math.cos(angle));
+        changeY = (float) (speed * Math.sin(angle));
+        // TODO: add changeZ
 
-				return;
-			}
-		}
+        rotateAngle = (int) ((angle * 180 / Math.PI) + 45 + 90);
+    }
 
-		if (maxRange) {
-			GameManager.get().getWorld().removeEntity(this);
-		}
-	}
-	
-	public void updatePos() {
-		maxRange = false;
-		if (RANGE < speed) {
-			setPosX(goalX);
-			setPosY(goalY);
-			setPosZ(goalZ);
-			maxRange = true;
-		} else {
-			setPosX(getPosX() + changeX);
-			setPosY(getPosY() + changeY);
-		}
-		RANGE -= speed;
-	}
-	
-	public float getRange() {
-		return RANGE;
-	}
-	
-	@Override
-	public float getDamage() {
-		return DAMAGE;
-	}
+    public int rotateAngle() {
+        return rotateAngle;
+    }
+
+    @Override
+    public void onTick(long time) {
+
+
+        updatePos();
+        rocketEffectTimer++;
+        if (rocketEffectTimer % 6 == 0) {
+            if (rocketCurrentSpriteIndexCount <= 2) {
+                setTexture(rocketSpriteArray[rocketCurrentSpriteIndexCount]);
+                if (rocketCurrentSpriteIndexCount < 3) {
+                    rocketCurrentSpriteIndexCount++;
+                }
+            }
+        }
+
+        Collection<AbstractEntity> entities = GameManager.get().getWorld().getEntities().values();
+        for (AbstractEntity entity : entities) {
+
+            if (entity instanceof EnemyEntity && this.collidesWith(entity)) {
+                ((EnemyEntity) entity).getShot(this);
+
+                GameManager.get().getWorld().removeEntity(this);
+
+                /**
+                 * Spawn explosion when projectile hits entity
+                 */
+                ExplosionProjectile exp = new ExplosionProjectile(goalX - (AOE_width / 2), goalY - (AOE_height / 2), 0,
+                        AOE_width, AOE_height, 0, AOE_width, AOE_height, aoeDAMAGE);
+                GameManager.get().getWorld().addEntity(exp);
+
+                return;
+            }
+        }
+
+        if (maxRange) {
+            GameManager.get().getWorld().removeEntity(this);
+        }
+    }
+
+    public void updatePos() {
+        maxRange = false;
+        if (RANGE < speed) {
+            setPosX(goalX);
+            setPosY(goalY);
+            setPosZ(goalZ);
+            maxRange = true;
+        } else {
+            setPosX(getPosX() + changeX);
+            setPosY(getPosY() + changeY);
+        }
+        RANGE -= speed;
+    }
+
+    /**
+     * Returns Range value
+     */
+    public float getRange() {
+        return RANGE;
+    }
+
+    /**
+     * Returns Damage value
+     */
+    @Override
+    public float getDamage() {
+        return DAMAGE;
+    }
+
+    /**
+     * Returns AOE Damage value
+     */
+    public float getAOEDamage() {
+        return aoeDAMAGE;
+    }
 
 }
