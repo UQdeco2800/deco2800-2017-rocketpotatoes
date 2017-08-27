5cb86bff293bb84d9553fafe25a062529928d057
Adam White <whitea0197@gmail.com>
2017-08-27 22:16:15 +1000

    Tidied AbstractEntity and fixed RocketPotatoes imports

--- potatoes/src/main/java/com/deco2800/potatoes/RocketPotatoes.java
+++ potatoes/src/main/java/com/deco2800/potatoes/RocketPotatoes.java
@@ -1,51 +1,26 @@
 package com.deco2800.potatoes;
 
 
-import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.renderers.BatchTiledMapRenderer;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.TimeUtils;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.deco2800.potatoes.entities.*;
-import com.deco2800.potatoes.handlers.MouseHandler;
-import com.deco2800.potatoes.managers.*;
-import com.deco2800.potatoes.observers.KeyDownObserver;
-import com.deco2800.potatoes.observers.ScrollObserver;
-import com.deco2800.potatoes.renderering.Render3D;
-import com.deco2800.potatoes.renderering.Renderable;
-import com.deco2800.potatoes.renderering.Renderer;
-import com.deco2800.potatoes.worlds.InitialWorld;
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
-import com.deco2800.potatoes.screens.GameScreen;
 import com.deco2800.potatoes.screens.MainMenuScreen;
 
 public class RocketPotatoes extends Game {
+	
 	/**
 	 * Called when the {@link Application} is first created.
 	 */
-	
-
+	@Override
 	public void create() {
 		this.setScreen(new MainMenuScreen(this));
 	}
 
+	@Override
 	public void render() {
 		super.render();
 	}
 
+	@Override
 	public void dispose() {
 	}
 }
