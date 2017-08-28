ccc59e6d874f48b23bac349c42f368b8aa4e9d8b
brakchen <jpchen1995@gmail.com>
2017-08-28 19:23:37 +1000

    can add a variety of different damage trees

--- potatoes/src/main/java/com/deco2800/potatoes/entities/trees/DamageTree.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/trees/DamageTree.java
@@ -5,13 +5,17 @@ import com.deco2800.potatoes.entities.TimeEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 
 public class DamageTree extends AbstractTree implements Tickable {
     private static final transient Logger LOGGER = LoggerFactory.getLogger(ResourceTree.class);
-    private static final transient String TEXTURE = "damage_tree";
+    private static final transient String TEXTURE = "lightning_tree";
+    private static final transient  Map<String, List<String>> LOADEDTEXTURE=Texture(TEXTURE);
+
 
     /**
      * Static field to store information about upgrades
@@ -23,8 +27,6 @@ public class DamageTree extends AbstractTree implements Tickable {
      */
     public DamageTree() {
     }
-
-
     /**
      * Base Constructor
      */
@@ -35,6 +37,40 @@ public class DamageTree extends AbstractTree implements Tickable {
     /**
      * Returns the list of upgrades for the tree
      */
+    private static List<String> damageTreeAnimation(String Type){
+        List<String> result=new LinkedList<>();
+        switch (Type){
+            case "lightning_tree":
+                for(int i=1;i<10;i++) result.add("lightning_tree"+i);
+                break;
+            case "Lightning_being_damaged":
+                for(int i=1;i<10;i++) result.add("Lightning_being_damaged"+i);
+                break;
+            case "Lightning_damaged":
+                for(int i=1;i<9;i++) result.add("Lightning_damaged"+i);
+                break;
+            case "Lightning_damaged_being_damaged":
+                for(int i=1;i<10;i++) result.add("Lightning_damaged_being_damaged"+i);
+                break;
+            case "Lightning_dead":
+                for(int i=1;i<8;i++) result.add("Lightning_dead"+i);
+                break;
+            default:
+                for(int i=1;i<10;i++) result.add("lightning_tree"+i);
+                break;
+
+        }
+        return result;
+    }
+
+
+    private static Map<String,List<String>> Texture(String treeType){
+        Map<String, List<String>> result = new HashMap<String, List<String>>();
+        result.put(treeType,damageTreeAnimation(treeType));
+        return result;
+    };
+
+
     @Override
     public List<UpgradeStats> getAllUpgradeStats() {
         return STATS;
@@ -58,5 +94,12 @@ public class DamageTree extends AbstractTree implements Tickable {
         }
 
         return result;
+    }
+    @Override
+    public String getTexture(){
+        //TODO: condition need to apply diff texture and diff status
+        return LOADEDTEXTURE.get(TEXTURE).get((int)Math.round((Math.random()+0.1)*9));
+
+
     }
 }
