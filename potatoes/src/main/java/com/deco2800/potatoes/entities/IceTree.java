fe2ac4718c670b5823963cbf09c232b0168b6d52
brakchen <jpchen1995@gmail.com>
2017-08-28 18:09:23 +1000

    create a IceTree extends DamageTree

--- potatoes/src/main/java/com/deco2800/potatoes/entities/IceTree.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/IceTree.java
@@ -1,4 +1,15 @@
 package com.deco2800.potatoes.entities;
 
-public class IceTree {
+import com.deco2800.potatoes.entities.trees.DamageTree;
+
+public class IceTree extends DamageTree {
+    private final static String TEXTURE = "ice_basic_tree";
+    public IceTree(){
+
+    }
+
+    public IceTree(float posX, float posY, float posZ){
+        super(posX, posY, posZ, TEXTURE,100,10);
+    }
+
 }
