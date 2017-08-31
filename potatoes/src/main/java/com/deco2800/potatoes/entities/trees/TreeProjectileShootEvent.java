2324563a50c655e6891c52f2c750f17a9dbd3eec
BenJamin Wildie (Comquack) <benwildie@hotmail.com>
2017-08-31 11:05:52 +1000

    Projectile spawns at centre of tile rather than corner

--- potatoes/src/main/java/com/deco2800/potatoes/entities/trees/TreeProjectileShootEvent.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/trees/TreeProjectileShootEvent.java
@@ -35,7 +35,7 @@ public class TreeProjectileShootEvent extends TimeEvent<AbstractTree> {
                 tree.getPosY());
         if (target1.isPresent() && (tree.distance(target1.get()) <= tree.getUpgradeStats().getRange())) {
 
-            GameManager.get().getWorld().addEntity(new BallisticProjectile(target1.get().getClass(),tree.getPosX(), tree.getPosY(), tree.getPosZ(),
+            GameManager.get().getWorld().addEntity(new BallisticProjectile(target1.get().getClass(),tree.getPosX()+0.5f, tree.getPosY()+0.5f, tree.getPosZ(),
                     target1, tree.getUpgradeStats().getRange(), 10, 10));
 //					GameManager.get().getWorld().addEntity(new HomingProjectile(tree.getPosX(), tree.getPosY(), tree.getPosZ(),
 //				target1, tree.getUpgradeStats().getRange(),50));
