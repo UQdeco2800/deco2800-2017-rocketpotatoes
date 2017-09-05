5af4ccb73881e757656344d82678274da093799b
Craig Harvey <craigharvei@gmail.com>
2017-09-05 21:36:07 +1000

    rolled potato down hill

--- potatoes/src/main/java/com/deco2800/potatoes/screens/GameScreen.java
+++ potatoes/src/main/java/com/deco2800/potatoes/screens/GameScreen.java
@@ -231,7 +231,7 @@ public class GameScreen implements Screen {
                 		new TankEnemy(15 + random.nextFloat()*10, 20 + random.nextFloat()*10, 0));
             }
 
-            GameManager.get().getWorld().addEntity(new GoalPotate(15, 10, 0));
+            GameManager.get().getWorld().addEntity(new GoalPotate(8, 20, 0));
 
             for(int i=0 ; i<3 ; i++) {
                 GameManager.get().getWorld().addEntity(
