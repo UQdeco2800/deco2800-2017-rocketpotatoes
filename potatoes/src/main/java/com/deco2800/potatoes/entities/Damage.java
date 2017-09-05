962172a91f1bf76d0b190e8236c6c4cfe094a0a2
brakchen <jpchen1995@gmail.com>
2017-09-05 21:01:46 +1000

    add three new class for damage tree: lightning ice acorn

---
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/Damage.java
@@ -0,0 +1,4 @@
+package com.deco2800.potatoes.entities.trees;
+
+public interface Damage {
+}
---
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/LightningTree.java
@@ -0,0 +1,13 @@
+package com.deco2800.potatoes.entities;
+
+import com.deco2800.potatoes.entities.trees.DamageTree;
+
+public class IceTree extends Damage {
+    private final static String TEXTURE = "ice_basic_tree";
+
+    public IceTree(){
+
+    }
+
+
+}
