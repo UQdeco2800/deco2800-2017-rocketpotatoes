564ba26481792cf7340a3433ddb3ecf69052bb46
Qishi Zheng <vincezheng4265@gmail.com>
2017-09-10 13:08:39 +1000

    commented broken code for tank enemy movement

--- potatoes/src/main/java/com/deco2800/potatoes/entities/enemies/TankEnemy.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/enemies/TankEnemy.java
@@ -107,135 +107,72 @@ public class TankEnemy extends EnemyEntity implements Tickable {
 	}
 
 
-	/**
-	 * Squirrel follows it's path.
-	 * Requests a new path whenever it collides with a staticCollideable entity
-	 * moves directly towards the player once it reaches the end of it's path
-	 * @param i
-	 */
-	@Override
-	public void onTick(long i) {
-		PlayerManager playerManager = GameManager.get().getManager(PlayerManager.class);
-		PathManager pathManager = GameManager.get().getManager(PathManager.class);
-
-		// check paths
-
-		//check collision
-		for (AbstractEntity entity : GameManager.get().getWorld().getEntities().values()) {
-			if (entity.isStaticCollideable() && this.getBox3D().overlaps(entity.getBox3D())) {
-				//collided with wall
-				path = pathManager.generatePath(this.getBox3D(), playerManager.getPlayer().getBox3D());
-				target = path.pop();
-				break;
-			}
-		}
-
-		// check that we actually have a path
-		if (path == null || path.isEmpty()) {
-			path = pathManager.generatePath(this.getBox3D(), playerManager.getPlayer().getBox3D());
-		}
-
-
-		//check if close enough to target
-		if (target != null && target.overlaps(this.getBox3D())) {
-			target = null;
-		}
-
-		//check if the path has another node
-		if (target == null && !path.isEmpty()) {
-			target = path.pop();
-		}
-
-		float targetX;
-		float targetY;
-
-
-		if (target == null) {
-			target = playerManager.getPlayer().getBox3D();
-		}
-
-		targetX = target.getX();
-		targetY = target.getY();
-
-		float deltaX = getPosX() - targetX;
-		float deltaY = getPosY() - targetY;
-
-		float angle = (float)(Math.atan2(deltaY, deltaX)) + (float)(Math.PI);
-
-		//flip sprite
-		if (deltaX + deltaY >= 0) {
-			this.setTexture(TEXTURE_LEFT);
-		} else {
-			this.setTexture(TEXTURE_RIGHT);
-		}
-
-		float changeX = (float)(speed * Math.cos(angle));
-		float changeY = (float)(speed * Math.sin(angle));
-
-		this.setPosX(getPosX() + changeX);
-		this.setPosY(getPosY() + changeY);
-	}
-
-	// @Override
-	// public void onTick(long i) {
-	//
-	// // //set the target of tankEnemy to the closest goal
-	// Optional<AbstractEntity> target = WorldUtil.getClosestEntityOfClass(goal,
-	// getPosX(), getPosY());
-	//
-	// // get the position of the target
-	// float goalX = target.get().getPosX();
-	// float goalY = target.get().getPosY();
-	//
-	//
-	// if(this.distance(target.get()) < speed) {
-	// this.setPosX(goalX);
-	// this.setPosY(goalY);
-	// return;
-	// }
-	//
-	//
-	// float deltaX = getPosX() - goalX;
-	// float deltaY = getPosY() - goalY;
-	//
-	// float angle = (float)(Math.atan2(deltaY, deltaX)) + (float)(Math.PI);
-	//
-	//
-	//
-	// float changeX = (float)(speed * Math.cos(angle));
-	// float changeY = (float)(speed * Math.sin(angle));
-	//
-	// Box3D newPos = getBox3D();
-	//
-	// newPos.setX(getPosX() + changeX);
-	// newPos.setY(getPosY() + changeY);
-	//
-	//
-	// Map<Integer, AbstractEntity> entities =
-	// GameManager.get().getWorld().getEntities();
-	// boolean collided = false;
-	// for (AbstractEntity entity : entities.values()) {
-	// if (!this.equals(entity) && !(entity instanceof Projectile) &&
-	// newPos.overlaps(entity.getBox3D()) ) {
-	// if(entity instanceof Tower) {
-	// //soundManager.playSound("ree1.wav");
-	// }
-	// collided = true;
-	// }
-	// }
-	//
-	// if (!collided) {
-	// setPosX(getPosX() + changeX);
-	// setPosY(getPosY() + changeY);
-	// // tankEnemy changes direction when moving towards tree/tower
-	//
-	// if(this.getPosX()>goalX){
-	// this.setTexture(TEXTURE);
-	// }
-	// else{
-	// this.setTexture(TEXTURE);
-	// }
-	// }
-	//
-	// }
+//	/**
+//	 * Squirrel follows it's path.
+//	 * Requests a new path whenever it collides with a staticCollideable entity
+//	 * moves directly towards the player once it reaches the end of it's path
+//	 * @param i
+//	 */
+//	@Override
+//	public void onTick(long i) {
+//		PlayerManager playerManager = GameManager.get().getManager(PlayerManager.class);
+//		PathManager pathManager = GameManager.get().getManager(PathManager.class);
+//
+//		// check paths
+//
+//		//check collision
+//		for (AbstractEntity entity : GameManager.get().getWorld().getEntities().values()) {
+//			if (entity.isStaticCollideable() && this.getBox3D().overlaps(entity.getBox3D())) {
+//				//collided with wall
+//				path = pathManager.generatePath(this.getBox3D(), playerManager.getPlayer().getBox3D());
+//				target = path.pop();
+//				break;
+//			}
+//		}
+//
+//		// check that we actually have a path
+//		if (path == null || path.isEmpty()) {
+//			path = pathManager.generatePath(this.getBox3D(), playerManager.getPlayer().getBox3D());
+//		}
+//
+//
+//		//check if close enough to target
+//		if (target != null && target.overlaps(this.getBox3D())) {
+//			target = null;
+//		}
+//
+//		//check if the path has another node
+//		if (target == null && !path.isEmpty()) {
+//			target = path.pop();
+//		}
+//
+//		float targetX;
+//		float targetY;
+//
+//
+//		if (target == null) {
+//			target = playerManager.getPlayer().getBox3D();
+//		}
+//
+//		targetX = target.getX();
+//		targetY = target.getY();
+//
+//		float deltaX = getPosX() - targetX;
+//		float deltaY = getPosY() - targetY;
+//
+//		float angle = (float)(Math.atan2(deltaY, deltaX)) + (float)(Math.PI);
+//
+//		//flip sprite
+//		if (deltaX + deltaY >= 0) {
+//			this.setTexture(TEXTURE_LEFT);
+//		} else {
+//			this.setTexture(TEXTURE_RIGHT);
+//		}
+//
+//		float changeX = (float)(speed * Math.cos(angle));
+//		float changeY = (float)(speed * Math.sin(angle));
+//
+//		this.setPosX(getPosX() + changeX);
+//		this.setPosY(getPosY() + changeY);
+//	}
 }
