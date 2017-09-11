dad862f5622f04894521ab4d79db183ac9227d15
brakchen <jpchen1995@gmail.com>
2017-09-11 18:56:49 +1000

    added runtime animation texture and setAnimation

--- potatoes/src/main/java/com/deco2800/potatoes/entities/trees/DamageTree.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/trees/DamageTree.java
@@ -2,19 +2,18 @@ package com.deco2800.potatoes.entities.trees;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Function;
 
 import com.deco2800.potatoes.entities.Damage;
 import com.deco2800.potatoes.entities.StatisticsBuilder;
 import com.deco2800.potatoes.entities.Tickable;
+import com.deco2800.potatoes.entities.animation.Animation;
+import com.deco2800.potatoes.entities.animation.AnimationFactory;
 
 
 public class DamageTree extends AbstractTree implements Tickable {
     public static transient String TEXTURE ;
     private Damage damageTreeType;
-    /**
-     * Static field to store information about upgrades
-     */
-
 
     /**
      * Default constructor for serialization
@@ -58,32 +57,43 @@ public class DamageTree extends AbstractTree implements Tickable {
 
 
 
-
-
     @Override
     public List<TreeStatistics> getAllUpgradeStats() {
 
         if(damageTreeType instanceof IceTree)
-            return generateTree("ice_basic_tree");
+            return generateTree("ice_basic_tree",x-> AnimationFactory.createSimpleTimeAnimation(100,new String[] {"ice_basic_tree"}));
         else if(damageTreeType instanceof AcornTree)
-            return generateTree("acorn_tree");
-        return generateTree("lightning_tree1");
+            return generateTree("acorn_tree",x-> AnimationFactory.createSimpleTimeAnimation(100,new String[] {"acorn_tree"}));
+        return generateTree("lightning_tree1",x-> AnimationFactory.createSimpleTimeAnimation(100,new String[] {
+                "lightning_tree1",
+                "lightning_tree2",
+                "lightning_tree3",
+                "lightning_tree4",
+                "lightning_tree5",
+                "lightning_tree6",
+                "lightning_tree7",
+                "lightning_tree8",
+                "lightning_tree9",
+        }));
     }
 
     /**
      * Static method to create the list of upgrades
+     * @param texture the tree texture
+     * @param animation the runtime animation texture
+     * @return
      */
-    private static List<TreeStatistics> generateTree(String texture) {
+    private static List<TreeStatistics> generateTree(String texture,Function<AbstractTree,Animation> animation) {
         List<TreeStatistics> result = new LinkedList<>();
 		/* UpgradeStats(Health, Shooting Time, Shooting Range, Construction/Upgrade Time, events, events, texture) */
         result.add(new StatisticsBuilder<AbstractTree>().setHealth(10).setAttackRange(8f).setBuildTime(5000)
-				.setBuildCost(1).setTexture(texture).addEvent(new TreeProjectileShootEvent(3000))
+				.setBuildCost(1).setTexture(texture).setAnimation(animation).addEvent(new TreeProjectileShootEvent(3000))
 				.createTreeStatistics());
 		result.add(new StatisticsBuilder<AbstractTree>().setHealth(20).setAttackRange(8f).setBuildTime(2000)
-				.setBuildCost(1).setTexture(texture).addEvent(new TreeProjectileShootEvent(2500))
+				.setBuildCost(1).setTexture(texture).setAnimation(animation).addEvent(new TreeProjectileShootEvent(2500))
 				.createTreeStatistics());
 		result.add(new StatisticsBuilder<AbstractTree>().setHealth(30).setAttackRange(8f).setBuildTime(2000)
-				.setBuildCost(1).setTexture(texture).addEvent(new TreeProjectileShootEvent(1500))
+				.setBuildCost(1).setTexture(texture).setAnimation(animation).addEvent(new TreeProjectileShootEvent(1500))
 				.createTreeStatistics());
 
         return result;
