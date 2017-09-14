b5bea1eb9d839782550d639f6046e4c6cc96a9de
Tan Xue Ying <xueyingt96@gmail.com>
2017-09-14 12:46:16 +1000

    Game Over Screen done with start and exit button. Start button is linked to main menu and the exit button will terminate the game. 'G' key is used to for easier testing. Might improve afterwards.

--- potatoes/src/main/java/com/deco2800/potatoes/gui/GameOverGui.java
+++ potatoes/src/main/java/com/deco2800/potatoes/gui/GameOverGui.java
@@ -8,42 +8,61 @@ import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.deco2800.potatoes.managers.GameManager;
 import com.deco2800.potatoes.managers.TextureManager;
 import com.deco2800.potatoes.screens.GameScreen;
+//import com.deco2800.potatoes.screens.GameOverScreen;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static com.badlogic.gdx.utils.Align.center;
 
-public class GameOverGui extends Gui {
+public class GameOverGui extends Gui { 
+	private GameScreen screen;
+//	private GameOverScreen gameOverScreen;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GameOverGui.class);
 
-    private GameScreen screen;
     private Stage stage;
-
-    // Buttons
     private Skin uiSkin;
-    private Drawable pauseMenuDrawable;
-    private Label gameOverLabel;
 
     private Table table;
+    // Buttons
+//    private Drawable pauseMenuDrawable;
+    private VerticalGroup primaryButtons;
+    private Drawable startDrawable;
+    private Drawable exitDrawable;
+    private ImageButton startButton;
+    private ImageButton exitButton;
+//    private Label gameOverLabel;
 
     public GameOverGui(Stage stage, GameScreen screen) {
-        this.screen = screen;
-        this.stage = stage;
+    	this.stage = stage;
+    	this.screen = screen;
 
-        uiSkin = new Skin(Gdx.files.internal("uiskin.json"));
+        uiSkin = new Skin(Gdx.files.internal("uiskin.json"));        
         table = new Table(uiSkin);
 
-        gameOverLabel = new Label("GAME OVER",uiSkin);
+//        gameOverLabel = new Label("GAME OVER",uiSkin);
+        // Make drawables from textures
+        startDrawable = new TextureRegionDrawable(new TextureRegion(GameManager.get().getManager(TextureManager.class).getTexture("startMainMenu")));
+        exitDrawable = new TextureRegionDrawable(new TextureRegion(GameManager.get().getManager(TextureManager.class).getTexture("exitMainMenu")));
+        startButton = new ImageButton(startDrawable);
+        exitButton = new ImageButton(exitDrawable);
+        
+        primaryButtons = new VerticalGroup();
+        primaryButtons.addActor(startButton);
+        primaryButtons.addActor(exitButton);
 
-        table.setBackground(pauseMenuDrawable);
-        table.add(gameOverLabel);
+        setupListeners();
+        
+        table.setBackground(new TextureRegionDrawable(new TextureRegion(GameManager.get().getManager(TextureManager.class).getTexture("gameOverScreen"))));
+        table.add(primaryButtons);
         table.setVisible(false);
-        table.setWidth(500);
-        table.setHeight(450);
+        table.setWidth(stage.getWidth());
+        table.setHeight(stage.getHeight());
         table.center();
         table.setPosition(stage.getWidth()/2,stage.getHeight()/2, center);
 
@@ -51,18 +70,40 @@ public class GameOverGui extends Gui {
 
     }
 
+    private void setupListeners() {
+        // Primary state
+
+        startButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                screen.menuBlipSound();
+                screen.exitToMenu();;
+            }
+        });
+
+        exitButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                screen.menuBlipSound();
+                // Todo nicer exit?
+                System.exit(0);
+            }
+        });
+
+    }
+
+
 
     @Override
-    public void show() {
+	public void show() {
         table.setVisible(true);
 
         stage.addActor(table);
     }
 
     @Override
-    public void hide() {
+	public void hide() {
         table.setVisible(false);
     }
-
 
 }
