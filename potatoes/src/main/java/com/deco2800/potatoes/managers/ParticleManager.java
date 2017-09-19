83590e0e97e8ad656c9b1b4f55c165a08ed71b74
Tom Richardson <tgrkzus@gmail.com>
2017-09-19 21:08:59 +1000

    Fixed clean emitter removal logic

--- potatoes/src/main/java/com/deco2800/potatoes/managers/ParticleManager.java
+++ potatoes/src/main/java/com/deco2800/potatoes/managers/ParticleManager.java
@@ -1,38 +1,42 @@
 package com.deco2800.potatoes.managers;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.deco2800.potatoes.renderering.particles.Particle;
 import com.deco2800.potatoes.renderering.particles.ParticleEmitter;
+import com.deco2800.potatoes.renderering.particles.types.BasicParticleType;
+import com.google.common.collect.Iterables;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class ParticleManager extends Manager {
+public class ParticleManager extends Manager implements TickableManager {
 
     private class EmitterContainer {
         public ParticleEmitter emitter;
         public float maxLifeTime;
         public float currentLifeTime;
+
+        public boolean toRemove;
     }
 
     // Emitters currently active
     List<EmitterContainer> emitters;
 
-    // Emitters waiting to stop
-    List<EmitterContainer> toDestroyEmitters;
-
     List<Particle> particlePool;
 
     public ParticleManager() {
         emitters = new ArrayList<>();
-        toDestroyEmitters = new ArrayList<>();
 
         // Initialize our pool of particles
         particlePool = new ArrayList<>();
         for (int i = 0; i < 1000000; ++i) {
             particlePool.add(new Particle());
         }
+
+        addParticleEmitter(5000.0f, new ParticleEmitter(50, 50,
+                new BasicParticleType(100000, 1000.0f, 100.0f, 64, Color.RED, 5, 5)));
     }
 
     public void addParticleEmitter(float lifeTime, ParticleEmitter e) {
@@ -46,7 +50,8 @@ public class ParticleManager extends Manager {
      * Ticks all active particle emitters and their particles
      * @param deltaTime tick delta
      */
-    public void onTick(double deltaTime) {
+    @Override
+    public void onTick(long deltaTime) {
         Iterator<EmitterContainer> emitterIterator = emitters.iterator();
 
         while (emitterIterator.hasNext()) {
@@ -55,12 +60,16 @@ public class ParticleManager extends Manager {
             e.emitter.onTick(deltaTime, particlePool);
             e.currentLifeTime += deltaTime;
 
-            if (e.currentLifeTime >= e.maxLifeTime) {
-                toDestroyEmitters.add(e);
-                emitterIterator.remove();
+            // If exceeded max (don't do anything if lifetime is set to unlimited)
+            if (e.toRemove || (e.currentLifeTime >= e.maxLifeTime && e.maxLifeTime != 0)) {
+                e.toRemove = true;
+                if (!e.emitter.hasParticles()) {
+                    emitterIterator.remove();
+                }
+
+                e.emitter.stop();
             }
         }
-        toDestroyEmitters.removeIf(emitter -> !emitter.emitter.hasParticles());
     }
 
     /**
@@ -89,8 +98,7 @@ public class ParticleManager extends Manager {
 
             if (e.emitter == emitter) {
                 e.emitter.stop();
-                toDestroyEmitters.add(e);
-                emitterIterator.remove();
+                e.toRemove = true;
                 return;
             }
         }
