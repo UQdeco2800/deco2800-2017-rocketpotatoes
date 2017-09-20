00adecd3d487ae537ff6ef704ec9da290c79f456
Michael Ruigrok <ruigrok.michael@gmail.com>
2017-09-20 23:15:37 +1000

    fixed sprite batch renaming from using '-' to '_'

--- devtools/3ds_to_sheet.sh
+++ devtools/3ds_to_sheet.sh
@@ -14,12 +14,12 @@ blender -b -P sprite_batcher.py -- "$1" "$stripped_file"
 (cd blender-output
 convert -delay 20 -alpha off "$stripped_file[0-9]*".png "$stripped_file.gif"
 montage "$stripped_file[0-9]*.png" -tile 4x2 -geometry 512x512+0+0 -background transparent "$2"
-mv "${stripped_file}000.png" ${stripped_file}-S.png
-mv "${stripped_file}001.png" ${stripped_file}-SE.png
-mv "${stripped_file}002.png" ${stripped_file}-E.png
-mv "${stripped_file}003.png" ${stripped_file}-NE.png
-mv "${stripped_file}004.png" ${stripped_file}-N.png
-mv "${stripped_file}005.png" ${stripped_file}-NW.png
-mv "${stripped_file}006.png" ${stripped_file}-W.png
-mv "${stripped_file}007.png" ${stripped_file}-SW.png
+mv "${stripped_file}000.png" ${stripped_file}_S.png
+mv "${stripped_file}001.png" ${stripped_file}_SE.png
+mv "${stripped_file}002.png" ${stripped_file}_E.png
+mv "${stripped_file}003.png" ${stripped_file}_NE.png
+mv "${stripped_file}004.png" ${stripped_file}_N.png
+mv "${stripped_file}005.png" ${stripped_file}_NW.png
+mv "${stripped_file}006.png" ${stripped_file}_W.png
+mv "${stripped_file}007.png" ${stripped_file}_SW.png
 )
