06c2e312c6b4d1d7d27cd5c0e9d49b04d0061e78
BenJamin Wildie (Comquack) <benwildie@hotmail.com>
2017-09-21 22:52:06 +1000

    added terain test

---
+++ potatoes/src/test/java/com/deco2800/potatoes/TerrainTest.java
@@ -0,0 +1,35 @@
+package com.deco2800.potatoes;
+
+import com.deco2800.potatoes.entities.Player;
+import com.deco2800.potatoes.managers.GameManager;
+import com.deco2800.potatoes.worlds.terrain.Terrain;
+import com.deco2800.potatoes.util.WorldUtil;
+import org.junit.Test;
+import org.junit.Before;
+import com.badlogic.gdx.Input;
+import static org.junit.Assert.*;
+
+public class TerrainTest {
+    Terrain t1;
+    Terrain t2;
+    Terrain t3;
+
+    @Before
+    public void setup() {
+        t1 = new Terrain("a",1f,true);
+        t2 = new Terrain("a",1f,true);
+        t2 = new Terrain("b",2f,false);
+    }
+
+    @Test
+    public void setTests(){
+        assertTrue(t1.getMoveScale()==1);
+        assertTrue(t1.isPlantable());
+    }
+    @Test
+    public void equalsTests(){
+        t1.equals(t1);
+        t1.equals(t2);
+        t1.equals(t3);
+    }
+}
--- potatoes/src/test/java/com/deco2800/potatoes/entities/PlayerTest.java
+++ potatoes/src/test/java/com/deco2800/potatoes/entities/PlayerTest.java
@@ -11,54 +11,54 @@ import org.junit.Before;
 import com.badlogic.gdx.Input;
 
 public class PlayerTest {
-	Player player;
+    Player player;
 
-	@Before
-	public void setup() {
-		player = new Player();
-		player = new Player(1,1,0);
-		PlayerManager m = new PlayerManager();
-		m.setPlayer(player);
-	}
-	
-	@Test
-	public void directionTest() {
-		player.getDirection();
-	}
+    @Before
+    public void setup() {
+        player = new Player();
+        player = new Player(1, 1, 0);
+        PlayerManager m = new PlayerManager();
+        m.setPlayer(player);
+    }
 
-	@Test
-	public void keysTest(){
-		player.handleKeyDown(Input.Keys.W);
-		player.handleKeyUp(Input.Keys.W);
-		player.handleKeyDown(Input.Keys.S);
-		player.handleKeyUp(Input.Keys.S);
-		player.handleKeyDown(Input.Keys.A);
-		player.handleKeyUp(Input.Keys.A);
-		player.handleKeyDown(Input.Keys.D);
-		player.handleKeyUp(Input.Keys.D);
-		player.handleKeyUp(Input.Keys.SPACE);
-		player.handleKeyUp(Input.Keys.SPACE);
-		player.handleKeyUp(Input.Keys.SPACE);
-		player.handleKeyUp(Input.Keys.R);
-		player.handleKeyUp(Input.Keys.ESCAPE);
-		player.handleKeyUp(Input.Keys.E);
-	}
+    @Test
+    public void directionTest() {
+        player.getDirection();
+    }
 
-	@Test
-	public void stringTest(){
-		player.toString();
-		player.getProgressBar();
-	}
-	
-	@Test
-	public void damageTest() {
-		player.addState(PlayerState.damaged);
-		player.hasState(PlayerState.damaged);
-	}
-	
-	@Test
-	public void checkKeyDownTest() {
-		player.handleKeyUp(Input.Keys.W);
-	}
+    @Test
+    public void keysTest() {
+        player.handleKeyDown(Input.Keys.W);
+        player.handleKeyUp(Input.Keys.W);
+        player.handleKeyDown(Input.Keys.S);
+        player.handleKeyUp(Input.Keys.S);
+        player.handleKeyDown(Input.Keys.A);
+        player.handleKeyUp(Input.Keys.A);
+        player.handleKeyDown(Input.Keys.D);
+        player.handleKeyUp(Input.Keys.D);
+        player.handleKeyUp(Input.Keys.SPACE);
+        player.handleKeyUp(Input.Keys.SPACE);
+        player.handleKeyUp(Input.Keys.SPACE);
+        player.handleKeyUp(Input.Keys.R);
+        player.handleKeyUp(Input.Keys.ESCAPE);
+        player.handleKeyUp(Input.Keys.E);
+    }
+
+    @Test
+    public void stringTest() {
+        player.toString();
+        player.getProgressBar();
+    }
+
+    @Test
+    public void damageTest() {
+        player.addState(PlayerState.damaged);
+        player.hasState(PlayerState.damaged);
+    }
+
+    @Test
+    public void checkKeyDownTest() {
+        player.handleKeyUp(Input.Keys.W);
+    }
 
 }
