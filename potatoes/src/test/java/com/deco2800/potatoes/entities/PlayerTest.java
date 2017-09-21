dae3a3bda1156e7fa167bd8457dfa02f08586ef0
BenJamin Wildie (Comquack) <benwildie@hotmail.com>
2017-09-21 18:34:42 +1000

    playertest improved

--- potatoes/src/test/java/com/deco2800/potatoes/entities/PlayerTest.java
+++ potatoes/src/test/java/com/deco2800/potatoes/entities/PlayerTest.java
@@ -2,7 +2,10 @@ package com.deco2800.potatoes.entities;
 
 import com.deco2800.potatoes.entities.Player;
 import com.deco2800.potatoes.entities.Player.PlayerState;
-
+import com.deco2800.potatoes.managers.GameManager;
+import com.deco2800.potatoes.managers.PlayerManager;
+import com.deco2800.potatoes.worlds.World;
+import com.deco2800.potatoes.util.WorldUtil;
 import org.junit.Test;
 import org.junit.Before;
 import com.badlogic.gdx.Input;
@@ -13,6 +16,9 @@ public class PlayerTest {
 	@Before
 	public void setup() {
 		player = new Player();
+		player = new Player(1,1,0);
+		PlayerManager m = new PlayerManager();
+		m.setPlayer(player);
 	}
 	
 	@Test
@@ -30,6 +36,12 @@ public class PlayerTest {
 		player.handleKeyUp(Input.Keys.A);
 		player.handleKeyDown(Input.Keys.D);
 		player.handleKeyUp(Input.Keys.D);
+		player.handleKeyUp(Input.Keys.SPACE);
+		player.handleKeyUp(Input.Keys.SPACE);
+		player.handleKeyUp(Input.Keys.SPACE);
+		player.handleKeyUp(Input.Keys.R);
+		player.handleKeyUp(Input.Keys.ESCAPE);
+		player.handleKeyUp(Input.Keys.E);
 	}
 
 	@Test
@@ -48,5 +60,5 @@ public class PlayerTest {
 	public void checkKeyDownTest() {
 		player.handleKeyUp(Input.Keys.W);
 	}
-	
+
 }
