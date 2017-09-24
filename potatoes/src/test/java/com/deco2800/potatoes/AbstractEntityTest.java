2a2d77202ec006b5ee1cdae1dde04f336a1d6bda
Neil Ashford <ashfordneil0@gmail.com>
2017-09-24 12:14:07 +1000

    Collision checks for Abstract Entity. Removed centered field, as well as any Box3D from this class. Also, renderLengths must now be specified, because CollisionMasks do not support length and width as concepts.

--- potatoes/src/test/java/com/deco2800/potatoes/AbstractEntityTest.java
+++ potatoes/src/test/java/com/deco2800/potatoes/AbstractEntityTest.java
@@ -1,5 +1,7 @@
 package com.deco2800.potatoes;
 
+
+import com.deco2800.potatoes.collisions.Box2D;
 import com.deco2800.potatoes.entities.AbstractEntity;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,20 +15,15 @@ public class AbstractEntityTest {
 
 		public TestableAbstractEntity() {};
 
-		public TestableAbstractEntity(float posX, float posY, float posZ, float xLength, float yLength, float zLength,
-				String texture) {
-			super(posX, posY, posZ, xLength, yLength, zLength, texture);
+		public TestableAbstractEntity(float posX, float posY, float xLength, float yLength, String texture) {
+
+            super(new Box2D(posX, posY, xLength, yLength), xLength, yLength, texture);
 		}
 
 
-		public TestableAbstractEntity(float posX, float posY, float posZ, float xLength, float yLength, float zLength,
-				float xRenderLength, float yRenderLength, String texture) {
-			super(posX, posY, posZ, xLength, yLength, zLength, xRenderLength, yRenderLength, texture);
-		}
-
-		public TestableAbstractEntity(float posX, float posY, float posZ, float xLength, float yLength, float zLength,
-				float xRenderLength, float yRenderLength, boolean centered, String texture) {
-			super(posX, posY, posZ, xLength, yLength, zLength, xRenderLength, yRenderLength, centered, texture);
+		public TestableAbstractEntity(float posX, float posY, float xLength, float yLength, float xRenderLength, 
+                float yRenderLength, String texture) {
+            super(new Box2D(posX, posY, xLength, yLength), xRenderLength, yRenderLength, texture);
 		}
 
 	}
@@ -35,21 +32,20 @@ public class AbstractEntityTest {
 
 	@Before
 	public void setUp() throws Exception {
-		abstractEntity = new TestableAbstractEntity(1, 2, 3, 4, 5, 6, "texture");
+		abstractEntity = new TestableAbstractEntity(1, 2, 4, 5, "texture");
 	}
 
 	//Common to all initialisation test
 	private void initTestCommon() {
+		assertEquals("getPosY() bad init ", 1f, abstractEntity.getPosX(), 0f);
 		assertEquals("getPosY() bad init ", 2f, abstractEntity.getPosY(), 0f);
-		assertEquals("getPosZ() bad init ", 3f, abstractEntity.getPosZ(), 0f);
-		assertEquals("getXLength() bad init ", 4f, abstractEntity.getXLength(), 0f);
-		assertEquals("getYLength() bad init ", 5f, abstractEntity.getYLength(), 0f);
-		assertEquals("getZLength() bad init ", 6f, abstractEntity.getZLength(), 0f);
+		// assertEquals("getXLength() bad init ", 4f, abstractEntity.getXLength(), 0f);
+		// assertEquals("getYLength() bad init ", 5f, abstractEntity.getYLength(), 0f);
 	}
 
 	@Test
 	public void initTest() {
-		abstractEntity = new TestableAbstractEntity(1, 2, 3, 4, 5, 6, "texture");
+		abstractEntity = new TestableAbstractEntity(1, 2, 4, 5, "texture");
 		initTestCommon();
 		assertEquals("getPosX() bad init ", 1f, abstractEntity.getPosX(), 0f);
 	}
@@ -70,18 +66,10 @@ public class AbstractEntityTest {
 
 	@Test
 	public void initTestVariableRendering() {
-		abstractEntity = new TestableAbstractEntity(1, 2, 3, 4, 5, 6, 7, 8, "texture");
+		abstractEntity = new TestableAbstractEntity(1, 2, 4, 5, 7, 8, "texture");
 		initTestCommon();
 		testGetRenderLength();
 		assertEquals("getPosX() bad init ", 1f, abstractEntity.getPosX(), 0f);
-	}
-
-	@Test
-	public void initTestCentred() {
-		abstractEntity = new TestableAbstractEntity(1, 2, 3, 4, 5, 6, 7, 8, true, "texture");
-		initTestCommon();
-		assertEquals("getPosX() bad init ", 1.5f, abstractEntity.getPosX(), 0f);
-		testGetRenderLength();
 	}
 
 	@Test
@@ -94,20 +82,20 @@ public class AbstractEntityTest {
 
 	@Test
 	public void equalsTest() {
-		TestableAbstractEntity original = new TestableAbstractEntity(1, 2, 3, 4, 5, 6, "texture");
-		TestableAbstractEntity imposter = new TestableAbstractEntity(1, 2, 3, 4, 5, 6, "texture");
+		TestableAbstractEntity original = new TestableAbstractEntity(1, 2, 3, 4, "texture");
+		TestableAbstractEntity imposter = new TestableAbstractEntity(1, 2, 3, 4, "texture");
 		assertTrue(original.equals(imposter));
 		assertTrue(imposter.equals(original));
 
 		//iterate through each parameter of AbstractEntity, changing one at a time
-		float p[] = {1, 2, 3, 4, 5, 6}; //parameters
-		for (int i = 0; i < 6; i++) {
+		float p[] = {1, 2, 3, 4}; //parameters
+		for (int i = 0; i < 4; i++) {
 			p[i] = i;
-			original = new TestableAbstractEntity(p[0], p[1], p[2], p[3], p[4], p[5], "texture");
+			original = new TestableAbstractEntity(p[0], p[1], p[2], p[3], "texture");
 			assertFalse(original.equals(imposter));
 			assertFalse(imposter.equals(original));
 			p[i] = i + 1;
-			original = new TestableAbstractEntity(p[0], p[1], p[2], p[3], p[4], p[5], "texture");
+			original = new TestableAbstractEntity(p[0], p[1], p[2], p[3], "texture");
 			assertTrue(p[i] + " is not " + (i + 1f) + " for " + i, original.equals(imposter));
 			assertTrue(p[i] + " is not " + (i + 1f) + " for " + i, imposter.equals(original));
 		}
