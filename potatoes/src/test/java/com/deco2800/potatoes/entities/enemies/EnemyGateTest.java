21a91c1f4cc3e53d3853fe46c0848b8ef69ee74a
Ryan Phelan <ryan_phelan@hotmail.com>
2017-09-24 20:11:11 +1000

    EnemyGateTest 100% code coverage

---
+++ potatoes/src/test/java/com/deco2800/potatoes/entities/enemies/EnemyGateTest.java
@@ -0,0 +1,52 @@
+package com.deco2800.potatoes.entities.enemies;
+
+import com.deco2800.potatoes.BaseTest;
+import com.deco2800.potatoes.managers.GameManager;
+import com.deco2800.potatoes.managers.WorldManager;
+import com.deco2800.potatoes.worlds.WorldType;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/*
+ * Test class to test the Moose enemy type
+ *
+ * @author ryanjphelan
+ */
+public class EnemyGateTest extends BaseTest {
+
+    private EnemyGate enemyGateEmpty;
+    private EnemyGate enemyGate1;
+
+    @Before
+    public void setup() throws Exception {
+        enemyGateEmpty = new EnemyGate();
+        enemyGate1 = new EnemyGate(0, 0, 0);
+        GameManager.get().getManager(WorldManager.class).setWorld(WorldType.FOREST_WORLD);
+    }
+
+    @After
+    public void cleanUp() {
+        GameManager.get().clearManagers();
+    }
+
+    /*
+     * Test the moose when it is initialised using the empty constructor
+     */
+    @Test
+    public void emptyConstructor() {
+        assertEquals("healthbar", enemyGateEmpty.getProgressBar().getTexture());
+    }
+
+    /*
+     * Test the toString method
+     */
+    @Test
+    public void toStringTest() {
+        Assert.assertEquals("String mismatch", "The Enemy Gate", enemyGate1.toString());
+    }
+
+}
