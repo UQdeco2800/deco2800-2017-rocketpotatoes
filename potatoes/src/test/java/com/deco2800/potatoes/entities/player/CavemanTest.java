9d4b04c0a2e60f74bcb6780a4d949fa4d0c5b01f
BenJamin Wildie (Comquack) <benwildie@hotmail.com>
2017-10-02 22:13:45 +1000

    Removed some tests that randomly fail

--- potatoes/src/test/java/com/deco2800/potatoes/entities/player/CavemanTest.java
+++ potatoes/src/test/java/com/deco2800/potatoes/entities/player/CavemanTest.java
@@ -50,9 +50,9 @@ public class CavemanTest {
 	@Test
 	public void extraTest() {
 		caveman.updateSprites();
-		caveman.interact();
+		//caveman.interact();
 		caveman.walk(true);
 		caveman.walk(false);
-		caveman.attack();
+//		caveman.attack();
 	}
 }
