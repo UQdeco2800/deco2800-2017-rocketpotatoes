d2bb30e9b93e397a24f9d847bdc8192eb493b286
Tom Richardson <tgrkzus@gmail.com>
2017-10-04 17:03:39 +1000

    Readded library init

--- potatoes/src/main/java/com/deco2800/potatoes/GameLauncher.java
+++ potatoes/src/main/java/com/deco2800/potatoes/GameLauncher.java
@@ -6,22 +6,33 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
+import com.sun.jna.*;
+
 /**
  * Launches RocketPotatoes.
- * 
+ *
  * @author leggy
  *
  */
 public class GameLauncher {
 	private GameLauncher() { }
 
+	public interface RLibrary extends Library {
+		RLibrary INSTANCE = (RLibrary) Native.loadLibrary("rustyfish", RLibrary.class);
+
+		void print_stuff(String stuff);
+	}
+
 	/**
 	 * Main function for the game
-	 * 
-	 * @param args
+	 *
+	 * @param arg
 	 *            Command line arguments (we wont use these)
 	 */
 	public static void main(String[] args) {
+		//Native.DEBUG_LOAD = true;
+		RLibrary.INSTANCE.print_stuff("Ayy I am the rust memelord");
+		/*
 		BasicConfigurator.configure();
 		Logger.getRootLogger().setLevel(Level.WARN); // We probably need a logger config file at some point
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
@@ -30,5 +41,6 @@ public class GameLauncher {
 		config.title = "DECO2800 2017: RocketPotatoes";
 		@SuppressWarnings("unused")
 		LwjglApplication game = new LwjglApplication(new RocketPotatoes(), config);
+		*/
 	}
 }
