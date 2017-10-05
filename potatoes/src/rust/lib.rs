0742d98beb33272e48daacf13566ac5f7848037e
Tom Richardson <tgrkzus@gmail.com>
2017-10-05 22:13:40 +1000

    Rust creates it's own opengl window/context and draws a nice box on it

--- potatoes/src/rust/lib.rs
+++ potatoes/src/rust/lib.rs
@@ -1,11 +1,13 @@
 #![crate_type = "dylib"]
 
+extern crate piston_window;
+use piston_window::{PistonWindow, WindowSettings};
+
 use std::ffi::{CStr, CString};
 use std::mem;
 use std::os::raw::c_char;
 use std::str;
-use std::thread;
-use std::time::Duration;
+use std::time::{Instant};
 
 /// Non windows callback
 #[cfg(not(windows))]
@@ -37,7 +39,6 @@ impl<T> Callback<T> {
 pub extern fn startGame(render: extern "C" fn(*const c_char)) {
     println!("Running game Linux");
     run_game(Callback { function: render });
-    thread::sleep(Duration::from_secs(1));
     println!("Game finished");
 }
 
@@ -48,7 +49,6 @@ pub extern fn startGame(render: extern "C" fn(*const c_char)) {
 pub extern fn startGame(render: extern "stdcall" fn(*const c_char)) {
     println!("Running game Win");
     run_game(Callback { function: render });
-    thread::sleep(Duration::from_secs(1));
     println!("Game finished");
 }
 
@@ -58,6 +58,33 @@ pub extern fn startGame(render: extern "stdcall" fn(*const c_char)) {
 /// and send back the frame to the java side to be drawn to the primary screen.
 pub fn run_game(render: Callback<*const c_char>) {
     render.call(to_ptr("Hey java".to_string()));
+
+    let mut window: PistonWindow = WindowSettings::new("Rustyfish", [1024, 784])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+
+    let timer = Instant::now();
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics| {
+
+            piston_window::clear([1.0; 4], graphics);
+
+            let width = 100.0;
+            let height = 100.0;
+            let elapsed = timer.elapsed();
+            let x = (context.get_view_size()[0] as f64 - width) / 2.0
+                * (1.0 + f64::sin(elapsed.as_secs() as f64 +
+                           elapsed.subsec_nanos() as f64 * 1e-9));
+            let y = context.get_view_size()[1] as f64 / 2.0;
+            piston_window::rectangle(
+                [1.0, 0.0, 0.0, 1.0],
+                [x, y, width, height],
+                context.transform,
+                graphics);
+        });
+    }
 }
 
 /// Converts native string to rust string
