ecccc40130628bd0c3fe31a8e6b1780fb0abc172
Adam White <whitea0197@gmail.com>
2017-10-16 22:39:32 +1000

    Added singleton design patter

--- potatoes/src/main/java/com/deco2800/potatoes/worlds/ForestWorld.java
+++ potatoes/src/main/java/com/deco2800/potatoes/worlds/ForestWorld.java
@@ -8,13 +8,19 @@ import com.deco2800.potatoes.worlds.terrain.TerrainType;
 import com.deco2800.potatoes.entities.AbstractEntity;
 
 public class ForestWorld extends WorldType {
-	
 	private static final TerrainType forestTerrain = new TerrainType(null, new Terrain(GRASS, 1, true), new Terrain(GROUND, 1, false), new Terrain(WATER, 0, false));
-	private List<Supplier<AbstractEntity>> forestEntities = defaultEntities("forest");
+	private static final List<Supplier<AbstractEntity>> forestEntities = defaultEntities("forest");
+
+	private static final ForestWorld instance = new ForestWorld(forestTerrain, forestEntities);
 	
-	public ForestWorld(TerrainType terrain, List<Supplier<AbstractEntity>> entities) {
-		terrain = forestTerrain;
-		entities = forestEntities;
-		clearSpots = clearPoints();
+	private ForestWorld(TerrainType terrain, List<Supplier<AbstractEntity>> entities) {
+		super(terrain, entities);
+	}
+
+	/**
+	 * Returns the singleton instance for this world type
+	 */
+	public static ForestWorld get() {
+		return instance;
 	}
 }
