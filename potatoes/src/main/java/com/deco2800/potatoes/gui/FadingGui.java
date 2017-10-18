9244f82ab6ea40e83c767e61328cab6338f6ce71
Dion Lao <dion.lao@uqconnect.edu.au>
2017-10-18 15:22:55 +1000

    Adding in particle effects

--- potatoes/src/main/java/com/deco2800/potatoes/gui/FadingGui.java
+++ potatoes/src/main/java/com/deco2800/potatoes/gui/FadingGui.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -12,9 +13,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.deco2800.potatoes.entities.Tickable;
 import com.deco2800.potatoes.managers.*;
+import com.deco2800.potatoes.renderering.Render3D;
 import com.deco2800.potatoes.renderering.particles.ParticleEmitter;
 import com.deco2800.potatoes.renderering.particles.types.BasicParticleType;
 import com.deco2800.potatoes.renderering.particles.types.BuoyantParticleType;
+import com.deco2800.potatoes.renderering.particles.types.ParticleType;
 
 //import java.awt.*;
 
@@ -44,22 +47,27 @@ public class FadingGui extends Gui implements Tickable{
         image =  new Image(new TextureRegionDrawable(new TextureRegion(textureManager
                 .getTexture(treeState.getTree().getTexture()))));
 
+        float x = (stage.getWidth() - image.getImageWidth())/2;
+        float y = (stage.getHeight() - image.getImageHeight())/2;
 
 
-        BasicParticleType particle = new BasicParticleType(50000, 1.0f * 1000.f,
-                100.0f,1024, Color.YELLOW, 3, 3);
-        particle.speed = 0.1f;
-        particle.alphaCeil = 0.5f;
-        particle.speedVarianceMin = 0.8f;
-        ParticleEmitter e = new ParticleEmitter(0, 0, particle);
-        GameManager.get().getManager(ParticleManager.class).addParticleEmitter(10000.0f, e);
+        ParticleManager p = GameManager.get().getManager(ParticleManager.class);
+        ParticleType particle =  new BasicParticleType(100000, 500.0f,
+                0.0f, 1024, Color.RED, 5, 5);
+        particle.speed = 0.9f;
+
+        Vector2 pos = Render3D.worldToScreenCoordinates(x,y, 0);
+        int tileWidth = (int) GameManager.get().getWorld().getMap().getProperties().get("tilewidth");
+        int tileHeight = (int) GameManager.get().getWorld().getMap().getProperties().get("tileheight");
+        p.addParticleEmitter(1.0f, new ParticleEmitter(pos.x + tileWidth / 2, pos.y + tileHeight / 2,
+                particle));
+
 
         container.add(unlockedLabel);
         container.row();
         container.add(image).size(100, 100);
         stage.addActor(container);
-        container.setPosition((stage.getWidth() - image.getImageWidth())/2, (stage
-                .getHeight() - image.getImageHeight())/2);
+        container.setPosition(x, y);
     }
 
     @Override
