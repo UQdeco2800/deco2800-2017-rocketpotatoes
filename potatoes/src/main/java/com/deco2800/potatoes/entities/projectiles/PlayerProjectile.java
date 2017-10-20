9e552614d538822305f5b6fc31c7b43bb1317f01
tristan_cargill <monshi10x@gmail.com>
2017-10-20 18:14:10 +1000

    Fixed player projectional projectiles

--- potatoes/src/main/java/com/deco2800/potatoes/entities/projectiles/PlayerProjectile.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/projectiles/PlayerProjectile.java
@@ -11,140 +11,154 @@ import com.deco2800.potatoes.util.WorldUtil;
 import java.util.Optional;
 
 public class PlayerProjectile extends Projectile {
-    protected float pPosX;
-    protected float pPosY;
-    protected float tPosX;
-    protected float tPosY;
-    protected String directions;
-    protected PlayerShootMethod shootingStyle;
+	protected float pPosX;
+	protected float pPosY;
+	protected float tPosX;
+	protected float tPosY;
+	protected String directions;
+	protected PlayerShootMethod shootingStyle;
 
-    public enum PlayerShootMethod {
-        DIRECTIONAL {
+	public enum PlayerShootMethod {
+		DIRECTIONAL {
 
-        },
-        HOMING {
+		},
+		HOMING {
 
-        },
-        BALLISTIC {
+		},
+		BALLISTIC {
 
-        }
+		}
 
-    }
+	}
 
+	public PlayerProjectile() {
+		// Blank comment to please the lord Sonar
+	}
 
-    public PlayerProjectile() {
-        //Blank comment to please the lord Sonar
-    }
+	/**
+	 * Creates a new projectile. A projectile is the vehicle used to deliver damage
+	 * to a target over a distance
+	 *
+	 * @param targetClass
+	 *            the targets class
+	 * @param startPos
+	 * @param targetPos
+	 * @param range
+	 * @param damage
+	 *            damage of projectile
+	 * @param projectileTexture
+	 *            the texture set to use for animations. Use ProjectileTexture._
+	 * @param startEffect
+	 *            the effect to play at the start of the projectile being fired
+	 * @param endEffect
+	 *            the effect to be played if a collision occurs
+	 * @param directions
+	 *            Player Directions
+	 * @param shootingStyle
+	 *            Directional, Homing or Ballistic?
+	 */
 
+	public PlayerProjectile(Class<?> targetClass, Vector3 startPos, Vector3 targetPos, float range, float damage,
+			ProjectileTexture projectileTexture, Effect startEffect, Effect endEffect, String directions,
+			PlayerShootMethod shootingStyle) {
+		super(targetClass, startPos, targetPos, range, damage, projectileTexture, startEffect, endEffect);
 
-    /**
-     * Creates a new projectile. A projectile is the vehicle used to deliver damage
-     * to a target over a distance
-     *
-     * @param targetClass       the targets class
-     * @param startPos
-     * @param targetPos
-     * @param range
-     * @param damage            damage of projectile
-     * @param projectileTexture the texture set to use for animations. Use ProjectileTexture._
-     * @param startEffect       the effect to play at the start of the projectile being fired
-     * @param endEffect         the effect to be played if a collision occurs
-     * @param directions        Player Directions
-     * @param shootingStyle     Directional, Homing or Ballistic?
-     */
+		this.pPosX = startPos.x;
+		this.pPosY = startPos.y;
+		this.tPosX = targetPos.x;
+		this.tPosY = targetPos.y;
+		this.directions = directions;
+		this.shootingStyle = shootingStyle;
+		ShootingStyle(shootingStyle);
+	}
 
-    public PlayerProjectile(Class<?> targetClass, Vector3 startPos, Vector3 targetPos, float range, float damage, ProjectileTexture projectileTexture, Effect startEffect,
-                            Effect endEffect, String directions, PlayerShootMethod shootingStyle) {
-        super(targetClass, startPos, targetPos, range, damage, projectileTexture, startEffect, endEffect);
+	@Override
+	public void onTick(long time) {
+		if ("HOMING".equalsIgnoreCase(shootingStyle.toString())) {
+			Optional<AbstractEntity> targetEntity = WorldUtil.getClosestEntityOfClass(targetClass, targetPos.x,
+					targetPos.y);
+			if (targetEntity.isPresent()) {
+				setTargetPosition(targetEntity.get().getPosX(), targetEntity.get().getPosY(),
+						targetEntity.get().getPosZ());
+			} else {
+				GameManager.get().getWorld().removeEntity(this);
+			}
+			updatePosition();
+		}
+		super.onTick(time);
+	}
 
-        this.pPosX = startPos.x;
-        this.pPosY = startPos.y;
-        this.tPosX = targetPos.x;
-        this.tPosY = targetPos.y;
-        this.directions = directions;
-        this.shootingStyle = shootingStyle;
-        ShootingStyle(shootingStyle);
+	/**
+	 * Returns selected shooting styles
+	 */
+	public PlayerProjectile.PlayerShootMethod getPlayerShootMethod() {
+		return shootingStyle;
+	}
 
-    }
+	public void ShootingStyle(PlayerShootMethod shootingStyle) {
 
+		/**
+		 * Shoots enemies base on the player direction
+		 */
+		if ("DIRECTIONAL".equalsIgnoreCase(shootingStyle.toString())) {
+			switch (directions.toLowerCase()) {
+			case "west":
+				setTargetPosition(pPosX - 5, pPosY - 5, 0);
+				updatePosition();
+				setPosition();
+				break;
+			case "east":
+				setTargetPosition(pPosX + 5, pPosY + 5, 0);
+				updatePosition();
+				setPosition();
+				break;
+			case "north":
+				setTargetPosition(pPosX + 15, pPosY - 15, 0);
+				updatePosition();
+				setPosition();
+				break;
+			case "south":
+				setTargetPosition(pPosX - 15, pPosY + 15, 0);
+				updatePosition();
+				setPosition();
+				break;
+			case "north-east":
+				setTargetPosition(pPosX + 15, pPosY + 1, 0);
+				updatePosition();
+				setPosition();
+				break;
+			case "north-west":
+				setTargetPosition(pPosX - 15, pPosY - 200, 0);
+				updatePosition();
+				setPosition();
+				break;
+			case "south-east":
+				setTargetPosition(pPosX + 20, pPosY + 200, 0);
+				updatePosition();
+				setPosition();
+				break;
+			case "south-west":
+				setTargetPosition(pPosX - 200, pPosY - 20, 0);
+				updatePosition();
+				setPosition();
+				break;
+			}
+		}
 
-    @Override
-    public void onTick(long time) {
-        if ("HOMING".equalsIgnoreCase(shootingStyle.toString())) {
-            Optional<AbstractEntity> targetEntity = WorldUtil.getClosestEntityOfClass(targetClass, targetPos.x, targetPos.y);
-            if (targetEntity.isPresent()) {
-                setTargetPosition(targetEntity.get().getPosX(), targetEntity.get().getPosY(), targetEntity.get().getPosZ());
-            } else {
-                GameManager.get().getWorld().removeEntity(this);
-            }
-            updatePosition();
-        }
-        super.onTick(time);
-    }
+	}
 
-    /**
-     * Returns selected shooting styles
-     */
-    public PlayerProjectile.PlayerShootMethod getPlayerShootMethod() {
-        return shootingStyle;
-    }
+	/**
+	 * Returns Target Pos X
+	 */
+	public float getTargetPosX() {
+		return tPosX;
+	}
 
-    public void ShootingStyle(PlayerShootMethod shootingStyle) {
-
-        /**
-         * Shoots enemies base on the player directions
-         */
-        if ("DIRECTIONAL".equalsIgnoreCase(shootingStyle.toString())) {
-            if (directions.equalsIgnoreCase("w")) {
-                setTargetPosition(pPosX - 5, pPosY - 5, 0);
-                updatePosition();
-                setPosition();
-            } else if (directions.equalsIgnoreCase("e")) {
-                setTargetPosition(pPosX + 5, pPosY + 5, 0);
-                updatePosition();
-                setPosition();
-            } else if ("n".equalsIgnoreCase(directions)) {
-                setTargetPosition(pPosX + 15, pPosY - 15, 0);
-                updatePosition();
-                setPosition();
-            } else if ("s".equalsIgnoreCase(directions)) {
-                setTargetPosition(pPosX - 15, pPosY + 15, 0);
-                updatePosition();
-                setPosition();
-            } else if ("ne".equalsIgnoreCase(directions)) {
-                setTargetPosition(pPosX + 15, pPosY + 1, 0);
-                updatePosition();
-                setPosition();
-            } else if ("nw".equalsIgnoreCase(directions)) {
-                setTargetPosition(pPosX - 15, pPosY - 200, 0);
-                updatePosition();
-                setPosition();
-            } else if ("se".equalsIgnoreCase(directions)) {
-                setTargetPosition(pPosX + 20, pPosY + 200, 0);
-                updatePosition();
-                setPosition();
-            } else if ("sw".equalsIgnoreCase(directions)) {
-                setTargetPosition(pPosX - 200, pPosY - 20, 0);
-                updatePosition();
-                setPosition();
-            }
-        }
-
-    }
-
-    /**
-     * Returns Target Pos X
-     */
-    public float getTargetPosX() {
-        return tPosX;
-    }
-
-
-    /**
-     * Returns Target Pos Y
-     */
-    public float getTargetPosY() {
-        return tPosY;
-    }
+	/**
+	 * Returns Target Pos Y
+	 */
+	public float getTargetPosY() {
+		return tPosY;
+	}
 
 }
