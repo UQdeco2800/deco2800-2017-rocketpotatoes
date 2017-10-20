45be3035dc3d6bbed5697b0f8dec004980162032
tristan_cargill <monshi10x@gmail.com>
2017-10-20 16:32:37 +1000

    Projectiles/effects are now individualized for tree types.

--- potatoes/src/main/java/com/deco2800/potatoes/entities/trees/TreeProjectileShootEvent.java
+++ potatoes/src/main/java/com/deco2800/potatoes/entities/trees/TreeProjectileShootEvent.java
@@ -6,9 +6,15 @@ import com.badlogic.gdx.math.Vector3;
 import com.deco2800.potatoes.entities.AbstractEntity;
 import com.deco2800.potatoes.entities.TimeEvent;
 import com.deco2800.potatoes.entities.effects.AOEEffect;
+import com.deco2800.potatoes.entities.effects.Effect.EffectTexture;
+import com.deco2800.potatoes.entities.effects.ExplosionEffect;
+import com.deco2800.potatoes.entities.effects.LazerEffect;
+import com.deco2800.potatoes.entities.effects.LightningEffect;
 import com.deco2800.potatoes.entities.enemies.EnemyEntity;
+import com.deco2800.potatoes.entities.health.MortalEntity;
 import com.deco2800.potatoes.entities.projectiles.BallisticProjectile;
-import com.deco2800.potatoes.entities.projectiles.Projectile;
+import com.deco2800.potatoes.entities.projectiles.HomingProjectile;
+import com.deco2800.potatoes.entities.projectiles.Projectile.ProjectileTexture;
 import com.deco2800.potatoes.managers.GameManager;
 import com.deco2800.potatoes.util.WorldUtil;
 
@@ -18,6 +24,8 @@ import com.deco2800.potatoes.util.WorldUtil;
  */
 public class TreeProjectileShootEvent extends TimeEvent<AbstractTree> {
 
+	private Enum<?> fireObjectType;
+	private Class<? extends AbstractEntity> fireObjectClass;
 	/**
 	 * Default constructor for serialization
 	 */
@@ -29,7 +37,9 @@ public class TreeProjectileShootEvent extends TimeEvent<AbstractTree> {
 	 * @param shootDelay
 	 *            the delay between shots
 	 */
-	public TreeProjectileShootEvent(int shootDelay) {
+	public TreeProjectileShootEvent(int shootDelay, Class<? extends AbstractEntity> fireObjectClass, Enum<?> fireObjectType) {
+		this.fireObjectType=fireObjectType;
+		this.fireObjectClass=fireObjectClass;
 		setDoReset(true);
 		setResetAmount(shootDelay);
 		reset();
@@ -40,13 +50,47 @@ public class TreeProjectileShootEvent extends TimeEvent<AbstractTree> {
 		Optional<AbstractEntity> target1 = WorldUtil.getClosestEntityOfClass(EnemyEntity.class, tree.getPosX(),
 				tree.getPosY());
 		if (target1.isPresent() && tree.distanceTo(target1.get()) <= tree.getUpgradeStats().getAttackRange()) {
-			GameManager.get().getWorld().addEntity(new BallisticProjectile(target1.get().getClass(),
-					new Vector3(tree.getPosX() + 0.5f, tree.getPosY() + 0.5f, tree.getPosZ()),
-					new Vector3(target1.get().getPosX(), target1.get().getPosY(), target1.get().getPosZ()),
-					tree.getUpgradeStats().getAttackRange(), 100, Projectile.ProjectileTexture.ROCKET, null,
-					new AOEEffect(target1.getClass(),
-							new Vector3(target1.get().getPosX(), target1.get().getPosY(), target1.get().getPosZ()), 1,
-							1)));
+			AbstractEntity e = null;
+			if(BallisticProjectile.class.isAssignableFrom(fireObjectClass)) {
+				e=new BallisticProjectile(target1.get().getClass(),
+						new Vector3(tree.getPosX() + 0.5f, tree.getPosY() + 0.5f, tree.getPosZ()),
+						new Vector3(target1.get().getPosX(), target1.get().getPosY(), target1.get().getPosZ()),
+						tree.getUpgradeStats().getAttackRange(), 100, (ProjectileTexture) fireObjectType, null,
+						new AOEEffect(target1.getClass(),
+								new Vector3(target1.get().getPosX(), target1.get().getPosY(), target1.get().getPosZ()), 1,
+								1));
+			}
+			else if(HomingProjectile.class.isAssignableFrom(fireObjectClass)) {
+				e=new HomingProjectile(target1.get().getClass(),
+						new Vector3(tree.getPosX() + 0.5f, tree.getPosY() + 0.5f, tree.getPosZ()),
+						new Vector3(target1.get().getPosX(), target1.get().getPosY(), target1.get().getPosZ()),
+						tree.getUpgradeStats().getAttackRange(), 100, (ProjectileTexture)fireObjectType, null,
+						new AOEEffect(target1.getClass(),
+								new Vector3(target1.get().getPosX(), target1.get().getPosY(), target1.get().getPosZ()), 1,
+								1));
+			}
+			else if(LightningEffect.class.isAssignableFrom(fireObjectClass)) {
+				e=new LightningEffect(EnemyEntity.class,
+						new Vector3(tree.getPosX(), tree.getPosY(), tree.getPosZ()),
+						new Vector3(target1.get().getPosX(), target1.get().getPosY(), target1.get().getPosZ()),
+						0.05f, 1, (EffectTexture)fireObjectType);
+			}
+			else if(LazerEffect.class.isAssignableFrom(fireObjectClass)) {
+				e=new LightningEffect(EnemyEntity.class,
+						new Vector3(tree.getPosX(), tree.getPosY(), tree.getPosZ()),
+						new Vector3(target1.get().getPosX(), target1.get().getPosY(), target1.get().getPosZ()),
+						0.05f, 1, (EffectTexture)fireObjectType);
+			}
+			else {
+				e=new BallisticProjectile(target1.get().getClass(),
+						new Vector3(tree.getPosX() + 0.5f, tree.getPosY() + 0.5f, tree.getPosZ()),
+						new Vector3(target1.get().getPosX(), target1.get().getPosY(), target1.get().getPosZ()),
+						tree.getUpgradeStats().getAttackRange(), 100, ProjectileTexture.LEAVES, null,
+						new ExplosionEffect(target1.getClass(),
+								new Vector3(target1.get().getPosX(), target1.get().getPosY(), target1.get().getPosZ()), 1,
+								1));
+			}
+			GameManager.get().getWorld().addEntity(e);
 
 //			GameManager.get().getWorld().addEntity(new HomingProjectile(target1.get().getClass(),
 //					new Vector3(tree.getPosX() + 0.5f, tree.getPosY() + 0.5f, tree.getPosZ()),
@@ -60,6 +104,6 @@ public class TreeProjectileShootEvent extends TimeEvent<AbstractTree> {
 
 	@Override
 	public TimeEvent<AbstractTree> copy() {
-		return new TreeProjectileShootEvent(getResetAmount());
+		return new TreeProjectileShootEvent(getResetAmount(),fireObjectClass,fireObjectType);
 	}
 }
