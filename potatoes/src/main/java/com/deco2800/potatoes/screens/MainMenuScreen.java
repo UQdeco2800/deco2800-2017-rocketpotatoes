ea313e3e6d28b3998f53df2bdd2b5bfe28d84255
Deanosity <dean.wyatt92@gmail.com>
2017-10-20 13:31:07 +1000

    Fix?

--- potatoes/src/main/java/com/deco2800/potatoes/screens/MainMenuScreen.java
+++ potatoes/src/main/java/com/deco2800/potatoes/screens/MainMenuScreen.java
@@ -9,16 +9,20 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.deco2800.potatoes.GameLauncher;
 import com.deco2800.potatoes.RocketPotatoes;
+import com.deco2800.potatoes.cheats.rust.Rustyfish;
 import com.deco2800.potatoes.gui.MainMenuGui;
 import com.deco2800.potatoes.managers.GameManager;
 import com.deco2800.potatoes.managers.MultiplayerManager;
 import com.deco2800.potatoes.managers.SoundManager;
 import com.deco2800.potatoes.managers.TextureManager;
 
+import com.sun.jna.Callback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Console;
 import java.net.InetAddress;
 import java.util.List;
 
@@ -31,6 +35,8 @@ import java.util.List;
 /**
  * Main menu screen implemetation. Handles the logic/display for the main menu, and other adjacent menus (e.g. options).
  * Also holds the logic for starting a game, (e.g. singleplayer, multiplayer, loaded, etc.)
+ *
+ * TODO make this nicer (i.e. use dispose) Probably has tiny memory leaks
  */
 public class MainMenuScreen implements Screen {
     private RocketPotatoes game;
