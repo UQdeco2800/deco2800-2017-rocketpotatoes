abe7e7a85b85fca7e91d4aaeb33d4f95898845cc
Neil Ashford <ashfordneil0@gmail.com>
2017-10-23 01:41:41 +1000

    removed warnings from rust code

--- potatoes/src/rust/util.rs
+++ potatoes/src/rust/util.rs
@@ -1,7 +1,6 @@
-use std::ffi::{CStr, CString};
+use std::ffi::CString;
 use std::mem;
 use std::os::raw::c_char;
-use std::str;
 use render::{RenderInfo, RenderLine, RenderRectangle, RenderObject};
 
 pub struct CallbackFunctions {
@@ -18,12 +17,6 @@ pub struct CallbackFunctions {
     pub draw_rectangle: extern "C" fn(RenderRectangle),
 }
 
-
-/// Converts native string to rust string
-pub fn to_string(pointer: *const c_char) -> String {
-    let slice = unsafe { CStr::from_ptr(pointer).to_bytes() };
-    str::from_utf8(slice).unwrap().to_string()
-}
 
 /// Converts rust string to native string
 pub fn to_ptr(string: String) -> *const c_char {
