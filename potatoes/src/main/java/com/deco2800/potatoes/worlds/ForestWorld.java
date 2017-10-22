9a180ba68c256c7b71b7b4758654c51e45fb3c9d
Joseph Sonny Seefeld <s2nny@outlook.com>
2017-10-22 17:09:55 +1000

    Added comments and cleaned code part II.

--- potatoes/src/main/java/com/deco2800/potatoes/worlds/ForestWorld.java
+++ potatoes/src/main/java/com/deco2800/potatoes/worlds/ForestWorld.java
@@ -10,7 +10,6 @@ import com.deco2800.potatoes.entities.AbstractEntity;
 public class ForestWorld extends WorldType {
 	private static final TerrainType forestTerrain = new TerrainType(null, new Terrain(GRASS, 1, true), new Terrain(GROUND, 1, false), new Terrain(WATER, 0, false));
 	private static final List<Supplier<AbstractEntity>> forestEntities = defaultEntities("forest");
-
 	private static final ForestWorld instance = new ForestWorld(forestTerrain, forestEntities);
 	
 	private ForestWorld(TerrainType terrain, List<Supplier<AbstractEntity>> entities) {
