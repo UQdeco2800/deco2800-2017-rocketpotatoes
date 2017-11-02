dc1eff8f8c5b8b14c7c2718b37af27ecf723cebe
Neil Ashford <ashfordneil0@gmail.com>
2017-11-02 12:16:26 +1000

    began rewriting the path manager

--- potatoes/src/main/java/com/deco2800/potatoes/managers/PathManager.java
+++ potatoes/src/main/java/com/deco2800/potatoes/managers/PathManager.java
@@ -2,138 +2,90 @@ package com.deco2800.potatoes.managers;
 
 import com.deco2800.potatoes.collisions.Shape2D;
 import com.deco2800.potatoes.collisions.Point2D;
-import com.deco2800.potatoes.entities.AbstractEntity;
-import com.deco2800.potatoes.util.Line;
-import com.deco2800.potatoes.util.MinimumSpanningTree;
-import com.deco2800.potatoes.util.Path;
+import com.deco2800.potatoes.collisions.Line2D;
+
 import com.deco2800.potatoes.worlds.World;
 
+import com.deco2800.potatoes.util.LRUCache;
+import com.deco2800.potatoes.util.RTree;
+import com.deco2800.potatoes.util.Pair;
 
-
-import java.util.*;
-
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
- * Object to manage the creation and allocation of paths for enemies to follow.
+ * Object to manage the allocation of paths for enemies to follow.
  */
-public class PathManager extends Manager implements ForWorld {
-    /* The PathManager stores a minimum spanning tree, representing all the internode connections that can be used to
-     * create paths. This is represented as a hashmap in memory, where the keys are nodes, and the values are the
-     * parent nodes of the keys within the minimum spanning tree.
-     *
-     * This tree is centered around the node storing the player's location for now, though in future that will be
-     * expanded so that multiple different goals can be set.
-     */
-    private Map<Point2D, Point2D> spanningTree;
-    private MinimumSpanningTree treeMaker;
-    private ArrayDeque<Point2D> nodes;
-    private ArrayDeque<Point2D> path;
-    private static final int NUMBER_OF_RANDOM_NODES = 100;
+public class PathManager extends Manager {
 
+    private int age;
+    private RTree<Integer> dots;
+    private Map<Pair<Integer>, Float> graph;
+    private LRUCache<TreeKey, Map<Integer, Integer>> trees;
+
+    private static int NUMBER_OF_RANDOM_NODES = 100;
 
     /**
-     * Basic constructor.
+     * Used to determine which shortest-path tree an enemy should follow.
      */
-    public PathManager() {
-        spanningTree = new HashMap<>();
-        nodes = new ArrayDeque<>();
-        path = new ArrayDeque<>();
+    private class TreeKey {
+        private final int age;
+        private final Shape2D goal;
+
+        /**
+         * Creates a new tree key.
+         *
+         * @param age
+         *      The age of the tree key.
+         * @param goal
+         *      The target or goal of the tree key.
+         */
+        public TreeKey(int age, Shape2D goal) {
+            this.age = age;
+            this.goal = goal;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof TreeKey)) {
+                return false;
+            }
+
+            TreeKey other = (TreeKey) o;
+            return age == other.age && goal.equals(other.goal);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(age, goal);
+        }
     }
 
     /**
-     * Populates the internal graph representation of the path manager, based on the initial world state.
-     * Should be run after loading the map
+     * Updates the graph, by removing lines that are already in use.
      */
+    private void updateGraph() {
+        age += 1;
+        graph = new HashMap<>();
 
-    public void initialise() {
+
+    }
+
+    public PathManager() {
+        age = -1;
+        dots = new RTree();
+        trees = new LRUCache(10); // TODO -- find a good value for this
 
         World world = GameManager.get().getWorld();
 
-        nodes.clear();
-        // Add place holder nodes at positions 0 and 1
-        // so that the player position and enemy position
-        // can be added later.
-        nodes.add(new Point2D(0, 0));   // Position 0 => player position.
-        nodes.add(new Point2D(0, 0));   // Position 1 => enemy position.
-
-        // Create obstacles from static entities.
-        // Initialise random points on the map to add as vertices of
-        // the graph.
-        for (int i = 0; i < NUMBER_OF_RANDOM_NODES; i++) {
-            nodes.add(new Point2D((float) (Math.random() * world.getWidth()), 
+        for (int i = 0; i < NUMBER_OF_RANDOM_NODES; ++i) {
+            dots.insert(i, new Point2D((float) (Math.random() * world.getWidth()),
                         (float) (Math.random() * world.getLength())));
         }
-
-        // Create a new minimum spanning tree
-        treeMaker = new MinimumSpanningTree(nodes.size());
-        // Add the nodes to the vertexList.
-        int i = 0;
-        for (Point2D node: nodes) {
-            treeMaker.addVertex(node, i++);
-        }
-
-        // Calculate edge weights in graph matrix
-        // based on static enemies.
-        treeMaker.initGraphWeightMatrix();
     }
 
-    /**
-     * Allocates a path to a given entity. Not guaranteed to be the optimal path, but at the time it is created it will
-     * have no collisions. Paths cannot be shared by multiple entities.
-     *
-     * @param start The starting point of the entity - where the path is going to begin.
-     * @param goal  The goal of the entity - where the path is going to end.
-     * @return The path object itself, which can then be followed.
-     */
-    public Path generatePath(Shape2D start, Shape2D goal) {
-
-        Point2D replaceStart = new Point2D(start.getX(), start.getY());
-        Point2D replaceGoal = new Point2D(goal.getX(), goal.getY());
-
-        path.clear();
-        Point2D next;
-        // Create line between start and goal.
-        Line line = new Line(start.getX(), start.getY(), goal.getX(), goal.getY());
-        // Check if this line has a clear path.
-        if (!collides(line)) {
-            // line is not obstructed.
-            //path.add(replaceStart);
-            path.add(replaceGoal);
-            return new Path(path);
-        }
-        // Check if the spanning tree has been initialise.
-        if (spanningTree.size() == 0) {
-            initialise();
-        }
-        // build the minimum spanning tree from the graph - and set the spanningTree variable
-        spanningTree = treeMaker.createTree(replaceGoal, replaceStart);
-        // Add the starting point to the path.
-
-        // Add extra path points as needed.
-        // Set next as the value returned from start as
-        // the key to spanningTree.
-        next = spanningTree.get(start);
-        while (next != null && !next.equals(goal)) {
-            path.add(next);
-            next = spanningTree.get(next);
-        }
-        path.add(replaceGoal);
-        return new Path(path);
-    }
-
-    private boolean collides(Line line) {
-        boolean output = false;
-
-        for (AbstractEntity e : GameManager.get().getWorld().getEntities().values()) {
-            if (e.isStatic() &&
-                    0 > e.getMask().distance(line.getEndPointOne().getX(), line.getEndPointOne().getY(), 
-                        line.getEndPointTwo().getX(), line.getEndPointTwo().getY())) {
-                output = true;
-                break;
-            }
-        }
-
-        return output;
+    public Point2D requestPath(Shape2D goal) {
+        return new Point2D(goal.getX(), goal.getY());
     }
 }
